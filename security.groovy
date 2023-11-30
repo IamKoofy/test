688c5457a0a2f25d82ee5ec3fd0c8a5b15a3272a
IamKoofy <abhishekj.1c@gmail.com>
2023-11-30 15:59:39 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -13,6 +13,9 @@ sub_min="-${INTERVAL} min"
 tim_cur=$(date -u "+%Y-%m-%d %H:%M:%S")
 tim_dif=$(date -u "+%Y-%m-%d %H:%M:%S" -d "$sub_min")
 
+# Initialize a variable to track if the exception was found
+EXCEPTION_FOUND=false
+
 # Loop through the pods (1-4)
 for PODdir in $(find "$EXCEPTION_LOG_PATH/$POD_NAME" -name "$EXCEPTION_LOGFILE" -type f -exec ls -t1 {} + | head -4 | awk -F'/' '{print $10}')
 do
@@ -29,15 +32,22 @@ do
         if [ "$nrec" -ge 1 ]; then
             # Count the number of exceptions within the specified time range
             EXCEPTION_COUNT=$(cat "$EXCEPTION_LOGFILE" | awk -v strec="$nrec" '{if (NR >= strec) print $0}' | grep -c "$EXCEPTION_STRING")
+
+            # Set EXCEPTION_FOUND to true if the exception is found
+            if [ "$EXCEPTION_COUNT" -ge "$THRESHOLD" ]; then
+                EXCEPTION_FOUND=true
+            fi
         fi
     else
         EXCEPTION_COUNT=0
     fi
-
-    # Check if the exception count exceeds the threshold
-    if [ "$EXCEPTION_COUNT" -ge "$THRESHOLD" ]; then
-        echo "FAILURE - $EXCEPTION_COUNT exception(s) found for the string '$EXCEPTION_STRING' in log path '$EXCEPTION_LOG_PATH/$POD_NAME/$PODdir/$EXCEPTION_LOGFILE'."
-    else
-        echo "SUCCESS - $EXCEPTION_COUNT exception(s) found for the string '$EXCEPTION_STRING' in log path '$EXCEPTION_LOG_PATH/$POD_NAME/$PODdir/$EXCEPTION_LOGFILE'. Exception(s) count is within the threshold."
-    fi
 done
+
+# Check if the exception was not found or is less than the threshold
+if [ "$EXCEPTION_FOUND" == false ]; then
+    echo "FAILURE - $EXCEPTION_STRING not found or less than $THRESHOLD times in the log files within the last $INTERVAL hours."
+    exit 1
+else
+    echo "SUCCESS - $EXCEPTION_STRING found and count is within the threshold."
+    exit 0
+fi
