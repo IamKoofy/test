2b7da3ad8d0e76a248ba56e7162af5142da83990
IamKoofy <abhishekj.1c@gmail.com>
2023-12-01 17:03:39 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,18 +1,33 @@
-    - name: Download Model Directory ZIP from Nexus
+{
+  "name": "Nexus Credential",
+  "description": "Custom Credential for Nexus Authentication",
+  "kind": "cloud",
+  "inputs": [
+    {
+      "id": "username",
+      "label": "Nexus Username",
+      "type": "string"
+    },
+    {
+      "id": "password",
+      "label": "Nexus Password",
+      "secret": true,
+      "type": "string"
+    }
+  ]
+}
+
+
+    - name: Get Nexus Credentials from AWX
+      set_fact:
+        nexus_credentials: "{{ lookup('ansible.builtin.credentials', 'your_nexus_credential', wantlist=True) }}"
+
+    - name: Download Model Directory ZIP from Nexus with Authentication
       ansible.builtin.uri:
         url: "{{ nexus_base_url }}/{{ nexus_repository_path }}/{{ model_zip_file }}"
         dest: "{{ extraction_folder }}/"
         follow_redirects: all
+        user: "{{ nexus_credentials.username }}"
+        password: "{{ nexus_credentials.password }}"
+        force_basic_auth: yes
       register: download_result
-
-    - name: Fail if download failed
-      ansible.builtin.fail:
-        msg: "Failed to download the Model Directory ZIP from Nexus"
-      when: download_result.status != 200
-
-    - name: Unzip Model Directory
-      ansible.builtin.unarchive:
-        src: "{{ extraction_folder }}/{{ model_zip_file }}"
-        dest: "{{ extraction_folder }}"
-        remote_src: false  # Important to specify this when unzipping from the control host
-
