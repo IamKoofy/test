5e627d314685c1acd06844cdefd7b92904b317c2
IamKoofy <abhishekj.1c@gmail.com>
2023-12-06 20:04:59 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,21 +1,12 @@
-def dockerBuildAndPush() {
-    // Replace 'your-image-name' with the actual repository name
-    def repository = env.REPOSITORY_NAME ?: 'your-image-name'
-    
-    // Docker build and push
-    script {
-        echo 'Building Docker Image...'
-        def buildNumber = currentBuild.number
-        def commitId = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
-        def imageTag = "${repository}:${commitId}-${buildNumber}"
-
-        def app = docker.build(imageTag, "-f Dockerfile .")
-        
-        echo 'Image Build Completed'
-        
-        // Push the Docker image to Nexus or your Docker registry
-        docker.withRegistry(env.NexusDockerRepo, 'NEXUS-REPO-CREDENTIALS') {
-            app.push()
-        }
+ // Return the image tag for later use
+        return imageTag
     }
+}
+
+def deployToOpenShift(deploymentScript = env.DEPLOYMENT_SCRIPT ?: 'default-deployment-script.sh', imageTag) {
+    echo "*** Deploying Application in AWS OpenShift ***"
+    echo "${imageTag}"
+
+    // Replace with the actual OpenShift deployment command
+    sh "${deploymentScript} -t ${env.AUTH_TOKEN} -n ${env.NAME_SPACE} -d ${DEPLOYMENT_CONFIG} -c ${DEPLOYMENT_CONFIG} -i ${env.NEXUS_REPO}/gtqc/${repository}:${imageTag}"
 }
