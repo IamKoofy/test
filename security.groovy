2203572d61ca9bdbc06ab1531c669f3b9dc52112
IamKoofy <abhishekj.1c@gmail.com>
2023-12-06 21:54:15 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,7 +1,10 @@
-def sonarScanMaven() {
-    // SonarQube analysis for Maven projects
-    withSonarQubeEnv('SonarQube-Container') {
-        echo "*** Analysing Code using SonarQube ***"
-        sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar -Dsonar.projectKey=' + env.SONAR_PROJECT_KEY + ' -Dsonar.projectName=' + env.SONAR_PROJECT_NAME
-    }
+def call() {
+    // Define the order of steps
+    mavenBuild()
+    snykscan()
+    nexusscan()
+    sonarScanMaven()
+    def imageTag = dockerBuildAndPush()
+    deployToOpenShift(imageTag: imageTag)
+    echo 'All stages completed.'
 }
