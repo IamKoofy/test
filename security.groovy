e7bb601a4e78b9037b03e0eec02e70d163f3c55e
IamKoofy <abhishekj.1c@gmail.com>
2023-12-06 19:45:05 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,62 +1,19 @@
-- name: Automate copy model folder to PV each sprint release
-  hosts: localhost
-  gather_facts: false
+def dockerBuildAndPush() {
+    // Replace 'your-image-name' with the actual repository name
+    def repository = env.REPOSITORY_NAME ?: 'your-image-name'
+    
+    // Docker build and push
+    script {
+        echo 'Building Docker Image...'
+        def buildNumber = currentBuild.number
+        def commitId = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+        def imageTag = "${repository}:${commitId}-${buildNumber}"
 
-  vars:
-    nexus_base_url: "{{ nexus_base_url | default('https://repos.nexus.com/repository') }}"
-    nexus_repository_path: "{{ nexus_repository_path | default('models') }}"
-    model_zip_file: "{{ model_zip_file | default('model_20221128-170018.zip') }}"
-    extraction_folder: "{{ extraction_folder | default('/Users/Model') }}"
-    oc_project_name: "{{ oc_project_name | default('hotel') }}"
-    oc_project_path: "{{ oc_project_path | default('/app/projects/Hotel/') }}"
-
-  tasks:
-
-- name: Copy the model directory to a pod containing "rasaniu"
-  ansible.builtin.shell: >
-    oc cp {{ extraction_folder }}/{{ model_directory }}/{{ model_subdirectory }}
-    {{ item.split()[0] }}:{{ oc_project_path }}
-  loop: "{{ pods_output.stdout_lines }}"
-  when: "'rasaniu' in item"
-
-    - name: Download Model Directory ZIP from Nexus with Authentication
-      ansible.builtin.uri:
-        url: "{{ nexus_base_url }}/{{ nexus_repository_path }}/{{ model_zip_file }}"
-        dest: "{{ extraction_folder }}/"
-        follow_redirects: all
-        user: "{{ nexus_username }}"
-        password: "{{ nexus_password }}"
-        force_basic_auth: yes
-      register: download_result
-
-    - name: Fail if download failed
-      ansible.builtin.fail:
-        msg: "Failed to download the Model Directory ZIP from Nexus"
-      when: download_result.status != 200
-
-    - name: Unzip Model Directory
-      ansible.builtin.unarchive:
-        src: "{{ extraction_folder }}/{{ model_zip_file }}"
-        dest: "{{ extraction_folder }}"
-        remote_src: false  # Important to specify this when unzipping from the control host
-
-    - name: Login to OpenShift
-      ansible.builtin.command: "oc login --server={{ server }} --token={{ openshift_token }}"
-      environment:
-        KUBECONFIG: "{{ extraction_folder }}/kubeconfig"  # Update the kubeconfig path
-
-    - name: Switch to the test project
-      ansible.builtin.command: "oc project {{ oc_project_name }}"
-
-    - name: Verify the path
-      ansible.builtin.command: "pwd"
-      args:
-        chdir: "{{ extraction_folder }}"
-
-    - name: List the pods
-      ansible.builtin.command: "oc get pods"
-      register: pods_output
-
-    - name: Copy the model directory to the first prod pod
-      ansible.builtin.shell: "oc rsync {{ extraction_folder }}/{{ model_directory }}/{{ model_subdirectory }} {{ pods_output.stdout_lines[1].split()[0] }}:{{ oc_project_path }}"
-      when: pods_output.stdout_lines | length > 1
+        def app = docker.build(imageTag, "-f Dockerfile .")
+        
+        echo 'Image Build Completed'
+        
+        // Push the Docker image to Nexus or your Docker registry
+        app.push()
+    }
+}
