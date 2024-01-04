42ba47a75e9b7c261747e32f36fdc484f4da12db
IamKoofy <abhishekj.1c@gmail.com>
2024-01-04 17:53:34 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,9 +1,6 @@
-// vars/startStop.groovy
-
 def call(Map params) {
     setBuildName(params)
     performEc2Actions(params)
-    displayEc2Instances(params)
 }
 
 def setBuildName(params) {
@@ -21,81 +18,77 @@ def performEc2Actions(params) {
 
 def performEc2ActionsAllInstances(params) {
     echo "Performing EC2 actions on all instances..."
-    def action = params.EC2_ACTION
+    def ec2Action = params.EC2_ACTION.toLowerCase()
+    def ec2Instances = getEc2Instances(params)
 
-    withAWS(credentials: "myteam-${params.AWS_ACCOUNT}-${params.AWS_ENVIRONMENT}", region: 'us-east-1') {
-        def instanceList = getEc2InstanceList(params)
-        echo "InstanceList before ${action}: ${instanceList}"
+    for (ec2Instance in ec2Instances) {
+        handleEc2Instance(ec2Instance, ec2Action, params)
+    }
+}
 
-        for (instanceID in instanceList) {
-            def instanceName = getEc2InstanceName(instanceID)
-            def instanceState = getEc2InstanceState(instanceID)
+def performEc2ActionsSelectedInstances(params) {
+    echo "Performing EC2 actions on selected instances..."
+    def ec2Action = params.EC2_ACTION.toLowerCase()
+    def ec2Instances = getEc2Instances(params)
 
-            echo "*** Instance Name of ${instanceID} is ${instanceName} ***"
-            echo "*** Instance State of ${instanceName} / ${instanceID} is ${instanceState} ***"
+    for (ec2Instance in ec2Instances) {
+        handleEc2Instance(ec2Instance, ec2Action, params)
+    }
+}
 
-            if ((action == 'stop' && instanceState == 'running') || (action == 'start' && instanceState == 'stopped')) {
-                try {
-                    timeout(time: 1, unit: 'MINUTES') {
-                        IS_APPROVED = input(message: "Are you OK to ${action} the instance ${instanceName} / ${instanceID}?",
-                                            parameters: [[$class: 'ChoiceParameterDefinition', choices: ['YES', 'NO'].join('\n'), name: 'Select YES / NO']])
-                    }
-                } catch (exc) {
-                    wrap([$class: 'BuildUser']) {
-                        error("${BUILD_USER} aborted the approval input to ${action} the instance")
-                    }
-                }
+def handleEc2Instance(ec2Instance, ec2Action, params) {
+    def instanceID = ec2Instance['instanceId']
+    def instanceName = ec2Instance['instanceName']
+    def instanceState = ec2Instance['instanceState']
 
-                if ("${IS_APPROVED}" == "YES") {
-                    echo "*** Instance ${instanceName} / ${instanceID} is going to be ${action} ***"
-                    sh "aws ec2 ${action}-instances --instance-ids ${instanceID}"
-                    def ec2Environment = params.AWS_ENVIRONMENT.toUpperCase()
-                    echo "*** EC2 Environment is ${ec2Environment} ***"
-                    wrap([$class: 'BuildUser']) {
-                        echo "*** Sending Slack Notification ***"
-                        slackSend channel: "${SLACK_CHANNEL}", color: (action == 'stop') ? "#FF7F50" : "#9FE2BF",
-                                  message: "${BUILD_USER} :: ${params.APP_NAME} :: ${ec2Environment} :: EC2 Instance ${instanceName} / ${instanceID} is ${action}:: ${BUILD_TIMESTAMP}"
-                    }
-                } else {
-                    echo "*** Instance ${instanceName} / ${instanceID} will not be ${action} as per user input ***"
-                }
-            } else {
-                echo "*** Instance ${instanceName} / ${instanceID} is already in ${action} state, so no further action is required ***"
+    echo "*** Instance Name of ${instanceID} is ${instanceName} ***"
+    echo "*** Instance State of ${instanceName} / ${instanceID} is ${instanceState} ***"
+
+    if ((instanceState == "running" && ec2Action == "stop") || (instanceState == "stopped" && ec2Action == "start")) {
+        try {
+            timeout(time: 1, unit: 'MINUTES') {
+                IS_APPROVED = input(message: "Are you OK to ${ec2Action} the instance ${instanceName} / ${instanceID}?",
+                                    parameters: [[$class: 'ChoiceParameterDefinition', choices: ['YES', 'NO'].join('\n'), name: 'Select YES / NO']])
+            }
+        } catch (exc) {
+            wrap([$class: 'BuildUser']) {
+                error("${BUILD_USER} aborted the approval input to ${ec2Action} the instance")
             }
         }
 
-        instanceList = getEc2InstanceList(params)
-        echo "InstanceList after ${action}: ${instanceList}"
+        if ("${IS_APPROVED}" == "YES") {
+            echo "*** Instance ${instanceName} / ${instanceID} is going to be ${ec2Action} ***"
+            sh "aws ec2 ${ec2Action}-instances --instance-ids ${instanceID}"
+            def ec2Environment = params.AWS_ENVIRONMENT.toUpperCase()
+            echo "*** EC2 Environment is ${ec2Environment} ***"
+            wrap([$class: 'BuildUser']) {
+                echo "*** Sending Slack Notification ***"
+                slackSend channel: "${SLACK_CHANNEL}", color: (ec2Action == 'stop') ? "#FF7F50" : "#9FE2BF",
+                          message: "${BUILD_USER} :: ${params.APP_NAME} :: ${ec2Environment} :: EC2 Instance ${instanceName} / ${instanceID} is ${ec2Action}:: ${BUILD_TIMESTAMP}"
+            }
+        } else {
+            echo "*** Instance ${instanceName} / ${instanceID} will not be ${ec2Action} as per user input ***"
+        }
+    } else {
+        echo "*** Instance ${instanceName} / ${instanceID} is already in ${instanceState} state, so no further action is required ***"
     }
 }
 
-def displayEc2Instances(params) {
-    echo "Displaying EC2 instances..."
+def getEc2Instances(params) {
+    def ec2Instances = []
     withAWS(credentials: "myteam-${params.AWS_ACCOUNT}-${params.AWS_ENVIRONMENT}", region: 'us-east-1') {
-        def instanceList = getEc2InstanceList(params)
-        echo "InstanceList : ${instanceList}"
+        sh "aws ec2 describe-instances --filters Name=tag-value,Values='${params.APP_NAME}' --output json > ./instance_list.json"
+        def instanceList = readJSON file: './instance_list.json'
 
-        for (instanceID in instanceList) {
-            def instanceName = getEc2InstanceName(instanceID)
-            def instanceState = getEc2InstanceState(instanceID)
-
-            echo "*** Instance Name of ${instanceID} is ${instanceName} ***"
-            echo "*** Instance State of ${instanceName} / ${instanceID} is ${instanceState} ***"
+        instanceList.Reservations.each { reservation ->
+            reservation.Instances.each { instance ->
+                def ec2Instance = [:]
+                ec2Instance['instanceId'] = instance.InstanceId
+                ec2Instance['instanceName'] = instance.Tags.find { it.Key == 'Name' }?.Value ?: instance.InstanceId
+                ec2Instance['instanceState'] = instance.State.Name
+                ec2Instances.add(ec2Instance)
+            }
         }
     }
-}
-
-def getEc2InstanceList(params) {
-    sh "aws ec2 describe-instances --filters Name=tag-value,Values='${params.APP_NAME}' --output text --query 'Reservations[*].Instances[*].InstanceId' > ./instance_id.list"
-    readFile(file: './instance_id.list').readLines()
-}
-
-def getEc2InstanceName(instanceID) {
-    sh "aws ec2 describe-instances --instance-id ${instanceID} --output text --query 'Reservations[*].Instances[*].{Instance_Name:Tags[?Key==`Name`]|[0].Value}'"
-            .trim()
-}
-
-def getEc2InstanceState(instanceID) {
-    sh "aws ec2 describe-instances --instance-id ${instanceID} --output text --query 'Reservations[*].Instances[*].State.Name'"
-            .trim()
+    return ec2Instances
 }
