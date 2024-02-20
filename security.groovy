93c4a9aa9e92f824a50b66e6ce55fc63719fad10
IamKoofy <abhishekj.1c@gmail.com>
2024-02-20 16:07:05 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -8,8 +8,11 @@ pipeline {
                     // Define the version as GIT+build_number
                     def version = "${env.GIT_COMMIT}+${env.BUILD_NUMBER}"
                     
+                    // Construct the path to the pom.xml file
+                    def pomPath = "${env.workspace}/${env.buildDir}/pom.xml"
+
                     // Update the version in the pom.xml file
-                    sh "sed -i 's/<artifactID>${param.COMPONENT}<\\/artifactID>/<artifactID>${param.COMPONENT}<\\/artifactID>\\n<version>${version}<\\/version>/' /path/to/pom.xml"
+                    sh "sed -i 's/<artifactID>${param.COMPONENT}<\\/artifactID>.*<version>[^<]*<\\/version>/<artifactID>${param.COMPONENT}<\\/artifactID>\\n<version>${version}<\\/version>/' ${pomPath}"
                 }
             }
         }
