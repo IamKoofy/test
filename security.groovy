2fccc1e83e06ef9e261a787d9de761766ced9567
IamKoofy <abhishekj.1c@gmail.com>
2024-02-20 15:34:09 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,27 +1,17 @@
-   - name: Check pod status
-      command: "kubectl get pod -n your_namespace {{ pod_name }} -o jsonpath='{.status.phase}'"
-      register: pod_status_output
-      changed_when: false
-      ignore_errors: true
+pipeline {
+    agent any
 
-    - name: Run shell script to check log status
-      command: "/path/to/your/shell/script.sh '{{ LOG_PATH }}' '{{ pod_name }}' '{{ LOG_FILE }}'"
-      register: log_check_result
-      ignore_errors: true
-
-    - name: Send email notification if no logs found or pod is not running
-      mail:
-        host: your_smtp_server
-        port: your_smtp_port
-        username: your_smtp_username
-        password: your_smtp_password
-        to: recipient@example.com
-        subject: "Alert: Pod {{ pod_name }} Status"
-        body: |
-          {% if log_check_result.rc != 0 %}
-          The log monitoring script detected no logs for pod {{ pod_name }} within the last 2 minutes.
-          {% endif %}
-          {% if pod_status_output.stdout != 'Running' %}
-          The pod {{ pod_name }} is not running. Current status: {{ pod_status_output.stdout }}.
-          {% endif %}
-      when: log_check_result.rc != 0 or pod_status_output.stdout != 'Running'
+    stages {
+        stage('Build and Update Version') {
+            steps {
+                script {
+                    // Define the version as GIT+build_number
+                    def version = "${env.GIT_COMMIT}+${env.BUILD_NUMBER}"
+                    
+                    // Update the version in the pom.xml file
+                    sh "sed -i 's/<artifactID>${param.COMPONENT}<\\/artifactID>/<artifactID>${param.COMPONENT}<\\/artifactID>\\n<version>${version}<\\/version>/' /path/to/pom.xml"
+                }
+            }
+        }
+    }
+}
