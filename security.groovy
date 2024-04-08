c751b09349d35b3f7df523a141f1e59dc619e0db
IamKoofy <abhishekj.1c@gmail.com>
2024-04-08 22:17:18 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,15 +1,11 @@
-- name: Rename backup directory
-  command: mv "{{ backup_dir }}" "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}"
-  when: env_var == "non-cde-dev" and oc_login_result.rc == 0
-
 - name: Push renamed backup folder to Git repository
-  git:
-    repo: "{{ github_repo_url }}/non-cde-dev/"
-    dest: "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}"
-    update: yes
-    force: yes  # Optional, force push even if remote updates are ahead
-    branch: master  # Specify the branch to push to, defaults to 'master'
+  shell: |
+    cd "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}"
+    git init
+    git add .
+    git commit -m "Backup commit"
+    git remote add origin "{{ github_repo_url }}/non-cde-dev/"
+    git push -u origin master
+  args:
+    executable: /bin/bash
   delegate_to: localhost
-  become: no  # No privilege escalation needed for pushing to Git
-  when: env_var == "non-cde-dev" and oc_login_result.rc == 0
-  register: git_push_result
