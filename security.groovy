3f0254286b3ff96143d064a3248815f4623b8059
IamKoofy <abhishekj.1c@gmail.com>
2024-04-08 22:47:52 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,11 +1,9 @@
-- name: Push renamed backup folder to Git repository
-  shell: |
-    cd "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}"
+ cd "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}"
     git init
+    git config --global user.name "{{ git_username }}"
+    git config --global user.email "{{ git_email }}"
     git add .
     git commit -m "Backup commit"
-    git remote add origin "{{ github_repo_url }}/non-cde-dev/"
-    git push -u origin master
-  args:
-    executable: /bin/bash
-  delegate_to: localhost
+    git remote add origin "https://{{ github_repo_url }}/non-cde-dev/"
+    git branch -m master main  # Rename master branch to main
+    git push -u origin main    # Push changes to main branch
