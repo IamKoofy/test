bb17e9c68d31b04269c537fdcff4ea0c4bbf379f
IamKoofy <abhishekj.1c@gmail.com>
2024-04-08 23:11:25 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,15 +1,7 @@
-- name: Push renamed backup folder to Git repository
-  git:
-    repo: "{{ github_repo_url }}/non-cde-dev/"
-    dest: "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}"
-    force: yes
-    push: yes
-    remote: origin
-    branch: main
-    accept_hostkey: yes  # If needed
-    username: "{{ git_username }}"
-    password: "{{ git_password }}"
-  delegate_to: localhost
-  become: no  # No privilege escalation needed for pushing to Git
-  when: env_var == "non-cde-dev" and oc_login_result.rc == 0
-  register: git_push_result
+git clone "{{ github_repo_url }}/non-cde-dev" /tmp/clone_repo
+
+# Copy the directory or files to the repository
+cp -r "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}" /tmp/clone_repo
+
+# Commit the changes
+cd /tmp/clone_repo
