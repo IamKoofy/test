fc8aa3a80d3c9c04dd95caa8608830169bcc427a
IamKoofy <abhishekj.1c@gmail.com>
2024-04-08 21:53:05 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,21 +1,15 @@
-- name: Push backup folder to Git repository
+- name: Rename backup directory
+  command: mv "{{ backup_dir }}" "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}"
+  when: env_var == "non-cde-dev" and oc_login_result.rc == 0
+
+- name: Push renamed backup folder to Git repository
   git:
-    repo: "{{ github_repo_url }}"
-    dest: "{{ backup_location }}/non-cde-dev/{{ namespace }}/folder_{{ ansible_date_time.iso8601_basic }}"
+    repo: "{{ github_repo_url }}/non-cde-dev/"
+    dest: "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}"
     update: yes
     force: yes  # Optional, force push even if remote updates are ahead
     branch: master  # Specify the branch to push to, defaults to 'master'
-    key: "{{ lookup('env', 'ANSIBLE_VAULT_PASSWORD_FILE') }}"  # Optional, use a vault password file
   delegate_to: localhost
   become: no  # No privilege escalation needed for pushing to Git
   when: env_var == "non-cde-dev" and oc_login_result.rc == 0
   register: git_push_result
-
-
-
-
-- name: Copy backup files to backup directory
-   copy:
-    src: "{{ backup_location }}"
-    dest: "{{ backup_location }}/non-cde-dev/{{ namespace }}/folder_{{ ansible_date_time.iso8601_basic }}"
-   when: env_var == "non-cde-dev" and oc_login_result.rc == 0
