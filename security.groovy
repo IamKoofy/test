a55daa7ff3af2eb3b0a9e3a314be4f2875648950
IamKoofy <abhishekj.1c@gmail.com>
2024-04-08 23:07:45 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,9 +1,15 @@
- cd "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}"
-    git init
-    git config --global user.name "{{ git_username }}"
-    git config --global user.email "{{ git_email }}"
-    git add .
-    git commit -m "Backup commit"
-    git remote add origin "https://{{ github_repo_url }}/non-cde-dev/"
-    git branch -m master main  # Rename master branch to main
-    git push -u origin main    # Push changes to main branch
+- name: Push renamed backup folder to Git repository
+  git:
+    repo: "{{ github_repo_url }}/non-cde-dev/"
+    dest: "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}"
+    force: yes
+    push: yes
+    remote: origin
+    branch: main
+    accept_hostkey: yes  # If needed
+    username: "{{ git_username }}"
+    password: "{{ git_password }}"
+  delegate_to: localhost
+  become: no  # No privilege escalation needed for pushing to Git
+  when: env_var == "non-cde-dev" and oc_login_result.rc == 0
+  register: git_push_result
