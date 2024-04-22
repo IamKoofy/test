442f1038ad98aa71bc9815ce77d513db30aa32f4
IamKoofy <abhishekj.1c@gmail.com>
2024-04-22 22:39:32 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,32 +1,40 @@
-- name: Push renamed backup folder to Git repository
-  shell: |
-    git clone "{{ git_url }}"
-    git config --global user.name "{{ github_username }}"
-    git config --global user.email "{{ github_username }}@test.com"
-    cd ePaaS-Backup/{{ env_var }}
+check pod status:
 
-    # Define the directory structure based on env_var
-    if [ "{{ env_var }}" == "non_cde_dev" ]; then
-      dir="non_cde/dev/{{ namespace }}"
-    elif [ "{{ env_var }}" == "non_cde_qa" ]; then
-      dir="non_cde/qa/{{ namespace }}"
-    else
-      dir=""
-    fi
+- name: Check Pod Status
+  k8s_info:
+    api_version: v1
+    kind: Pod
+    namespace: "{{ namespace }}"
+    name: "{{ pod_name }}"
+  register: pod_info
 
-    # Create directory if not exists
-    if [ ! -d "$dir" ]; then
-      mkdir -p "$dir"
-    fi
+- name: Debug Pod Info
+  debug:
+    var: pod_info
 
-    # Copy backup folder
-    cp -r "/tmp/{{ namespace }}_backup_{{ ansible_date_time.iso8601_basic }}" "$dir"
 
-    # Add, commit, and push changes
-    git add .
-    git commit -m "Backup commit"
-    git push -u origin main
-  no_log: true
-  args:
-    executable: /bin/bash
-  delegate_to: localhost
+- name: Restart Pod
+  k8s:
+    definition:
+      apiVersion: v1
+      kind: Pod
+      metadata:
+        name: "{{ pod_name }}"
+        namespace: "{{ namespace }}"
+      spec:
+        containers:
+        - name: "{{ pod_name }}" 
+    state: restarted
+
+
+- name: Wait for Pod to Be Running
+  wait_for:
+    timeout: "{{ pod_restart_timeout }}"
+    sleep: 5
+    host: "{{ pod_name }}"
+    port: 80
+    state: started
+
+
+main.yml
+pod_restart_timeout: 300 
