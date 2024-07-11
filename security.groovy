0cefaff7278f19dcf764e50440c338d3e6c3abbf
IamKoofy <abhishekj.1c@gmail.com>
2024-07-11 17:24:03 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,8 +1,7 @@
-{
-  "incident": {
-    "environment": "QA",
-    "username": "john_doe",
-    "incident_ticket_number": "INC123456",
-    "action": "unlock"
-  }
-}
+https://docs.github.com/en/enterprise-cloud@latest/admin/enforcing-policies/enforcing-policies-for-your-enterprise/enforcing-policies-for-github-actions-in-your-enterprise
+
+  grants unrestricted access to any public action or reusable workflow on GitHub. There isn't a dedicated section explaining this specific option because it's considered a less secure approach.
+
+2. "Allow enterprise, and select non-enterprise, actions and reusable workflows"
+
+This option offers more granular control and is explained through two related sections in the official documentation:
