74d7bc928840ead329107e6564bf271c414a8dce
IamKoofy <abhishekj.1c@gmail.com>
2024-08-01 18:07:33 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,10 +1,4 @@
-- name: Unlock or lock user accounts via GoAPI
-  hosts: localhost
-  gather_facts: no
-  vars:
-    api_user: "{{ lookup('env', 'API_USER') }}"
-    api_password: "{{ lookup('env', 'API_PASSWORD') }}"
-  tasks:
+tasks:
     - name: Set API URL based on environment
       set_fact:
         api_url: >-
@@ -18,28 +12,6 @@
             'https://dev.aoapi.myaxt.com:8001/go/rest/gacmd/v1/webusers/' + username
           }}
 
-    - name: Set payload for unlock action
-      set_fact:
-        payload: >-
-          {{
-            {'enabled': true}
-            if action == 'unlock'
-            else
-            {'enabled': false}
-          }}
-
-    - name: Perform the action on the user account
-      uri:
-        url: "{{ api_url }}"
-        method: PUT
-        user: "{{ api_user }}"
-        password: "{{ api_password }}"
-        body: "{{ payload | to_json }}"
-        headers:
-          Content-Type: "application/json"
-        status_code: 200
-      delegate_to: localhost
-      register: result
-
-    - debug:
-        var: result
+    - name: Debug API URL to verify environment setting
+      debug:
+        msg: "API URL is set to {{ api_url }}"
