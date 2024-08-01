ff641ddca39999b7222169388d1014b3d2d99dff
IamKoofy <abhishekj.1c@gmail.com>
2024-08-01 17:07:39 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,60 +1,36 @@
 ---
-- name: Handle Freshservice Incident
+- name: Unlock user accounts via GoAPI
   hosts: localhost
   gather_facts: no
-
   vars:
-    api_endpoints:
-      dev: "https://dev.api.example.com:8001/go/rest/gacmd/v1/webusers/"
-      qa: "https://qa.api.example.com:8001/go/rest/gacmd/v1/webusers/"
-      prod: "https://prod.api.example.com:8001/go/rest/gacmd/v1/webusers/"
-
+    api_user: "{{ lookup('env', 'API_USER') }}"
+    api_password: "{{ lookup('env', 'API_PASSWORD') }}"
   tasks:
-    - name: Parse incident details
+    - name: Set API URL based on environment
       set_fact:
-        environment: "{{ environment | lower }}"
-        action: "{{ action | lower }}"
-        username: "{{ username }}"
-        incident_ticket_number: "{{ incident_ticket_number }}"
-
-    - name: Validate input
-      fail:
-        msg: "Invalid environment '{{ environment }}' provided."
-      when: api_endpoints[environment] is not defined
-
-    - name: Set API endpoint
-      set_fact:
-        api_url: "{{ api_endpoints[environment] }}{{ username }}"
-
-    - name: Prepare payload for unlock/lock
-      set_fact:
-        payload: >
+        api_url: >-
           {{
-            {
-              "enabled": "true" if action == "unlock" else "false"
-            }
+            'https://aoapi.myaxt.com:8001/go/rest/gacmd/v1/webusers/' + username
+            if environment == 'QA'
+            else
+            'https://prod.aoapi.myaxt.com:8001/go/rest/gacmd/v1/webusers/' + username
+            if environment == 'PROD'
+            else
+            'https://dev.aoapi.myaxt.com:8001/go/rest/gacmd/v1/webusers/' + username
           }}
 
-    - name: Get API credentials
-      set_fact:
-        api_user: "{{ lookup('env', 'API_USER') }}"
-        api_pass: "{{ lookup('env', 'API_PASS') }}"
-
-    - name: Encode credentials
-      set_fact:
-        basic_auth: "{{ api_user }}:{{ api_pass }} | b64encode }}"
-
-    - name: Call API to unlock/lock account
+    - name: Unlock the user account
       uri:
         url: "{{ api_url }}"
         method: PUT
-        body: "{{ payload | to_json }}"
+        user: "{{ api_user }}"
+        password: "{{ api_password }}"
+        body: "{{ lookup('file', 'templates/unlock_account.json') | format(username=username) }}"
         headers:
-          Authorization: "Basic {{ basic_auth }}"
           Content-Type: "application/json"
-        status_code: [200, 201, 204]
-      register: api_result
+        status_code: 200
+      delegate_to: localhost
+      register: result
 
-    - name: Handle API response
-      debug:
-        msg: "User {{ username }} has been {{ action }}ed successfully in {{ environment }} environment. Incident ticket number: {{ incident_ticket_number }}"
+    - debug:
+        var: result
