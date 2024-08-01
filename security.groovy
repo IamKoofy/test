62606debc3d0482ef696192d3a0dd6f7c30c61d4
IamKoofy <abhishekj.1c@gmail.com>
2024-08-01 18:12:49 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,17 +1,14 @@
-tasks:
-    - name: Set API URL based on environment
+- name: Set API URL for QA environment
       set_fact:
-        api_url: >-
-          {{
-            'https://aoapi.myaxt.com:8001/go/rest/gacmd/v1/webusers/' + username
-            if environment == 'QA'
-            else
-            'https://prod.aoapi.myaxt.com:8001/go/rest/gacmd/v1/webusers/' + username
-            if environment == 'PROD'
-            else
-            'https://dev.aoapi.myaxt.com:8001/go/rest/gacmd/v1/webusers/' + username
-          }}
+        api_url: "https://aoapi.myaxt.com:8001/go/rest/gacmd/v1/webusers/{{ username }}"
+      when: environment == 'QA'
 
-    - name: Debug API URL to verify environment setting
-      debug:
-        msg: "API URL is set to {{ api_url }}"
+    - name: Set API URL for PROD environment
+      set_fact:
+        api_url: "https://prod.aoapi.myaxt.com:8001/go/rest/gacmd/v1/webusers/{{ username }}"
+      when: environment == 'PROD'
+
+    - name: Set API URL for DEV environment (default)
+      set_fact:
+        api_url: "https://dev.aoapi.myaxt.com:8001/go/rest/gacmd/v1/webusers/{{ username }}"
+      when: environment not in ['QA', 'PROD']
