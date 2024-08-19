0d15175d99a3dcc34fdf5b3bff56e611fed57b08
IamKoofy <abhishekj.1c@gmail.com>
2024-08-19 19:56:27 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,11 +1,18 @@
-- name: Pass hardcoded values to AWX template
-  awx.awx.job_template_launch:
-    name: "{{ awx_template_name }}"  # Your AWX template name here
-    extra_vars:
-      cluster: "{{ accounts_info.cluster }}"
-      username: "{{ accounts_info.username }}"
-      action: "{{ accounts_info.action }}"
+- name: Launch AWX job template using API with Basic Auth
+  uri:
+    url: "https://{{ awx_host }}/api/v2/job_templates/{{ awx_template_id }}/launch/"
+    method: POST
+    headers:
+      Content-Type: "application/json"
+    body_format: json
+    body:
+      extra_vars:
+        cluster: "{{ accounts_info.cluster }}"
+        username: "{{ accounts_info.username }}"
+        action: "{{ accounts_info.action }}"
+    status_code: 201
+    validate_certs: no  # Set to 'yes' if using valid SSL certificates
+    url_username: "{{ awx_username }}"  # Basic Auth username
+    url_password: "{{ awx_password }}"  # Basic Auth password
+    force_basic_auth: yes
   register: awx_job_result
-
-- debug:
-    msg: "AWX job result: {{ awx_job_result }}"
