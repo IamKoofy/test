d29db8a806337165b2f84ae8ded71e5f51cf240a
IamKoofy <abhishekj.1c@gmail.com>
2024-08-21 18:51:27 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,7 +1,10 @@
-{{
+- name: Set facts for account information
+  set_fact:
+    accounts_info: >-
+      {{
         ticket_data.results | map(attribute='json.requested_items') |
-        selectattr(0, 'defined') | 
-        map(attribute=0) | 
+        selectattr(0, 'defined') |
+        map(attribute=0) |
         map(attribute='custom_fields') |
-        map('combine', [{'cluster': item.environment, 'username': item.account_name_to_be_unlocked, 'action': 'unlock'}]) | list
+        map('combine', [{'cluster': _.environment, 'username': _.account_name_to_be_unlocked, 'action': 'unlock'}]) | list
       }}
