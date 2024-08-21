0016a77578594159c659a8d201d7ab1fe24d7b30
IamKoofy <abhishekj.1c@gmail.com>
2024-08-21 19:18:45 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,10 +1,5 @@
-{%- set accounts = [] -%}
-      {%- for item in ticket_data.results -%}
-        {%- set requested_items = item.json.requested_items -%}
-        {%- for req_item in requested_items -%}
-          {%- set fields = req_item.custom_fields -%}
-          {%- set account_info = {'cluster': fields.environment, 'username': fields.account_name_to_be_unlocked, 'action': 'unlock'} -%}
-          {%- do accounts.append(account_info) -%}
-        {%- endfor -%}
-      {%- endfor -%}
-      {{ accounts }}
+{{
+        ticket_data.results | map(attribute='json.requested_items') | 
+        map('extract', ['custom_fields.environment', 'custom_fields.platform', 'custom_fields.account_name_to_be_unlocked']) |
+        map('combine', [{'cluster': item[0], 'platform': item[1], 'username': item[2], 'action': 'unlock'} for item in _]) | list
+      }}
