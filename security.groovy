b41b15ca3d1af028671317cda6ade9f4bcf137d4
IamKoofy <abhishekj.1c@gmail.com>
2024-08-21 20:27:38 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,10 +1,12 @@
-{{
-        ticket_data.results[0].json.requested_items |
-        map(attribute='custom_fields') |
-        map(lambda cf: {
-          'environment': cf.environment,
-          'platform': cf.platform,
-          'account_name_to_be_unlocked': cf.account_name_to_be_unlocked
-        }) |
-        list
+- name: Parse the JSON content from ticket_data
+  set_fact:
+    json_data: "{{ ticket_data.results[0].content | from_yaml }}"
+
+- name: Extract fields from the JSON data
+  set_fact:
+    extracted_info: >-
+      {{
+        json_data.requested_items[0].custom_fields | dict2items |
+        selectattr('key', 'in', ['environment', 'platform', 'account_name_to_be_unlocked']) |
+        items2dict
       }}
