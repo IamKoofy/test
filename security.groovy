2d8b1194b8919e3de0796307712eed2377649ce7
IamKoofy <abhishekj.1c@gmail.com>
2024-08-26 22:23:03 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,6 +1,36 @@
- - name: Set fact for incident ID
-      set_fact:
-        incident_id_fact: "{{ item }}"  # Store the incident ID in a fact
-      loop: "{{ incident_ids }}"
-      loop_control:
-        label: "{{ item }}"
+---
+- name: Monitor Windows Services
+  hosts: windows
+  gather_facts: no
+
+  tasks:
+    - name: Check Windows services
+      win_shell: |
+        $services = @(
+            'queuereaderservice.exe',
+            'C:\CTE\.NET CORE\TestORG.exe',
+            'QueueProcessorShell.exe'
+        )
+        
+        $serviceStatuses = foreach ($service in $services) {
+            $status = Get-Process -Name $service -ErrorAction SilentlyContinue
+            if (-not $status) {
+                $service
+            }
+        }
+
+        if ($serviceStatuses.Count -gt 0) {
+            $emailParams = @{
+                From = "monitor@yourdomain.com"
+                To = "yourDL@domain.com"
+                Subject = "Service Not Running Alert"
+                Body = "The following services are not running: $($serviceStatuses -join ', ')"
+                SmtpServer = "smtp.yourdomain.com"
+            }
+            Send-MailMessage @emailParams
+        }
+      register: service_check_output
+
+    - name: Debug service check output
+      debug:
+        var: service_check_output
