5c2b2c408a2c83a061ceea54a0d83d6e952f8b0e
IamKoofy <abhishekj.1c@gmail.com>
2024-08-27 20:40:00 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -7,24 +7,30 @@
     - name: Check Windows services
       win_shell: |
         $services = @(
-            'queuereaderservice.exe',
-            'C:\CTE\.NET CORE\TestORG.exe',
-            'QueueProcessorShell.exe'
+            'queuereaderservice',  # Removed .exe
+            'TestORG',  # Removed full path and .exe
+            'QueueProcessorShell'  # Removed .exe
         )
         
-        $serviceStatuses = foreach ($service in $services) {
+        $serviceStatuses = @()  # Array to store service statuses
+
+        foreach ($service in $services) {
             $status = Get-Process -Name $service -ErrorAction SilentlyContinue
-            if (-not $status) {
-                $service
+            if ($status) {
+                $serviceStatuses += "Service $service is running."
+            } else {
+                $serviceStatuses += "Service $service is NOT running."
             }
         }
 
-        if ($serviceStatuses.Count -gt 0) {
+        $serviceStatuses -join "`n"  # Output statuses line by line
+
+        if ($serviceStatuses -match "NOT running") {
             $emailParams = @{
                 From = "monitor@yourdomain.com"
                 To = "yourDL@domain.com"
                 Subject = "Service Not Running Alert"
-                Body = "The following services are not running: $($serviceStatuses -join ', ')"
+                Body = "The following services have issues: `n $($serviceStatuses -join '`n')"
                 SmtpServer = "smtp.yourdomain.com"
             }
             Send-MailMessage @emailParams
@@ -33,4 +39,4 @@
 
     - name: Debug service check output
       debug:
-        var: service_check_output
+        var: service_check_output.stdout_lines  # Print output line by line
