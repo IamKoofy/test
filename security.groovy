9485e31182aa16b7f1eee1a4c87664ebbe266609
IamKoofy <abhishekj.1c@gmail.com>
2024-08-28 22:12:55 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,43 +1,20 @@
 ---
-- name: Main playbook
-  hosts: localhost
-  gather_facts: no
-  vars_prompt:
-    - name: playbook_choice
-      prompt: Enter ENVIRONMENT (DEV_CDE, DEV_NONCDE_CREDS, QA_CDE, QA_NONCDE, PROD_CDE, PROD_NONCDE)
-      private: no
-    - name: VPC_ID
-      prompt: "Please enter the VPC ID"
-      private: no
-    - name: alb_type
-      prompt: Please enter the type of ALB (internet or intranet)
-      private: no
-    - name: aws_region
-      prompt: Please enter the AWS Region
-      private: no
-    - name: security_groups
-      prompt: Please enter Security Groups (comma-separated)
-      private: no
-  
-  vars_files:
-    - aws_credentials.yml
-    - vars/vpc_credentials.yml
-  
-  tasks:
-    - name: Set ALB scheme based on user input
-      set_fact:
-        alb_scheme: "{{ 'internet-facing' if alb_type == 'internet' else 'internal' }}"
+- name: Tasks for DEV_CDE
+  amazon.aws.elb_application_lb:
+    name: "test-alb"
+    state: present
+    region: "{{ aws_region }}"
+    subnets:
+      - "subnet-04aab667b1c223ecd"
+      - "subnet-0ba426hgbe16400c"
+      - "subnet-07efe88bsdeda2f"
+    security_groups: "{{ security_groups.split(',') }}"
+    VPC_ID: "{{ VPC_ID }}"
+    listeners:
+      - Protocol: HTTP
+        Port: 80
+  register: alb_result
 
-    - name: Set AWS credentials based on VPC ID
-      set_fact:
-        aws_credential_key: "{{ vpc_credentials[VPC_ID] }}"
-
-    - name: Set AWS access and secret keys
-      set_fact:
-        aws_access_key: "{{ aws_credential_key.aws_access_key }}"
-        aws_secret_key: "{{ aws_credential_key.aws_secret_key }}"
-        aws_region: "{{ aws_region }}"
-
-    - name: Execute sub-playbook based on choice
-      include_tasks: "tasks_for_{{ playbook_choice }}.yml"
-      when: playbook_choice in ['DEV_CDE', 'DEV_NONCDE_CREDS', 'QA_CDE', 'QA_NONCDE', 'PROD_CDE', 'PROD_NONCDE']
+- name: Output ALB DNS name
+  debug:
+    msg: "ALB DNS Name: {{ alb_result.load_balancer.DNSName }}"
