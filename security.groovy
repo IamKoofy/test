d70271e7f7db98d63a3f870fa547e545f4e29858
IamKoofy <abhishekj.1c@gmail.com>
2024-09-05 01:01:21 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,5 +1,1 @@
-{% if user_check_response.status == 401 %}
-          Account {{ username }} is not found/incorrect please verify or check with MFT team
-          {% elif result.status != 200 %}
-          Account {{ username }} unlock could not be completed, MFT team in CC will investigate further and reach out to you
-          {% endif %}
+        "body": "{% if user_check_response.status == 401 %}Account {{ username }} is not found/incorrect, please verify or check with MFT team{% elif result.status != 200 %}Account {{ username }} unlock could not be completed, MFT team in CC will investigate further and reach out to you{% endif %}",
