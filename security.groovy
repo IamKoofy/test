0bc130e1aa1360cd74978755758ce531e6effddf
IamKoofy <abhishekj.1c@gmail.com>
2024-09-05 18:42:38 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,1 +1,10 @@
-        "body": "{% if user_check_response.status == 401 %}Account {{ username }} is not found/incorrect, please verify or check with MFT team{% elif result.status != 200 %}Account {{ username }} unlock could not be completed, MFT team in CC will investigate further and reach out to you{% endif %}",
+- task: PowerShell@2
+  inputs:
+    targetType: 'inline'
+    script: |
+      $majorVersion = "${{ parameters.MajorVersion }}"
+      $minorVersion = "${{ parameters.MinorVersion }}"
+      $buildNumber = "${{ parameters.BuildNumber }}"
+      .\nbgv cloud --version "$majorVersion.$minorVersion.$buildNumber"
+  workingDirectory: 'D:\BuildAgents\1\_work\_temp\data-test'
+  displayName: Set Version
