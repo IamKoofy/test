a3024c931d85b13622968cedfd7fec8815ae4e0f
IamKoofy <abhishekj.1c@gmail.com>
2024-09-05 20:01:16 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -9,9 +9,7 @@ task-groups/version-use-buildnumber.taskgroup.yml:
 - name: BuildNumber
   type: string
 
-# Rev is no longer needed since we won't include it
-#- name: Rev
-#  type: string
+# Removed the Rev parameter since we no longer need it
 
 steps:
 
@@ -20,15 +18,28 @@ steps:
   inputs:
     targetType: 'inline'
     script: |
+      $majorVersion="${{ parameters.MajorVersion }}"
+      $minorVersion="${{ parameters.MinorVersion }}"
+      $buildNumber="${{ parameters.BuildNumber }}"
+
       Write-Host "Versioning based on the following:-"
-      Write-Host "MajorVersion: $(MajorVersion)"
-      Write-Host "MinorVersion: $(MinorVersion)"
-      Write-Host "BuildNumber: $(BuildNumber)"
+      Write-Host "`tMajor Version: $majorVersion, Minor Version: $minorVersion, Build Number: $buildNumber`n"
 
-      $version = "$(MajorVersion).$(MinorVersion).$(BuildNumber)"
+      # Handle auto generation for BuildNumber if needed
+      if ($buildNumber.Trim().ToLower() -eq 'auto') {
+        $year = Get-Date -format yy
+        $dayNumber = (Get-Date).DayOfYear.ToString().PadLeft(3, '0')
+        $buildNumber = "$year$dayNumber"
+      }
 
-      # Regex to find and update version numbers in AssemblyInfo or other version files
+      # Construct the version as Major.Minor.Build
+      $newBuildNumber = "$majorVersion.$minorVersion.$buildNumber"
+
+      # Regex to find and update the version numbers in AssemblyInfo or other version files
       $versionRegex = "\d+\.\d+\.\d+"
+
+      # Update the build number in Azure DevOps
+      Write-Host "##vso[build.updatebuildnumber]$newBuildNumber"
 
       # Update the .NET assemblies with the new version number
       Write-Host "Searching for .NET assemblies ..."
@@ -37,11 +48,11 @@ steps:
                foreach { gci -Path $_.FullName -Recurse -Include AssemblyInfo.* }
 
       if ($files.count -gt 0) {
-        Write-Host "Applying $version to the following file(s):"
+        Write-Host "Applying $newBuildNumber to the following file(s):"
         foreach ($file in $files) {
           $filecontent = Get-Content $file
           attrib $file -r
-          $filecontent -replace $versionRegex, $version | Out-File $file
+          $filecontent -replace $versionRegex, $newBuildNumber | Out-File $file
           Write-Host "`t$file"
         }
       } else {
