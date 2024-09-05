092a54d924f3db798cb261d75d4f2ea26cedbf56
IamKoofy <abhishekj.1c@gmail.com>
2024-09-06 02:41:08 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,7 +1,6 @@
 - name: Parse XML and extract email
       set_fact:
-        extracted_email: "{{ api_response.content | from_xml | json_query('webusers.webuser.email') }}"
-
+xmlparse | xmlselect('//webusers/webuser/email') | first }}
     - name: Print extracted email
       debug:
         msg: "Extracted email: {{ extracted_email }}"
