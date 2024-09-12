406ad2fc2ccec240a62973c23726486f2a0be1aa
IamKoofy <abhishekj.1c@gmail.com>
2024-09-12 20:43:00 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,44 +1,58 @@
----
-- name: ALB Create
-  hosts: localhost
-  gather_facts: no
+#!/bin/bash
 
-  vars_files:
-    - aws_credentials.yml
-    - vars/vpc_credentials.yml
+# Shell Script to trigger alert if the exception match string is found in logs within the last 5 minutes
 
-  tasks:
-    # Assuming alb_name and playbook_choice are passed as extra vars from the previous step
-    - name: Set ALB scheme based on input
-      set_fact:
-        alb_scheme: "{{ 'internet-facing' if alb_type == 'internet' else 'internal' }}"
+EXCEPTION_LOG_PATH=$1
+EXCEPTION_STRING=$2
+THRESHOLD=$3
+EXCEPTION_LOGFILE=$4
+INTERVAL=${5:-5} # Default to 5 minutes if not provided
 
-    - name: Set AWS credentials based on VPC ID
-      set_fact:
-        aws_credential_key: "{{ vpc_credentials[VPC_ID] }}"
+# Calculate time-related variables
+sub_min="-${INTERVAL} min"
+tim_cur=$(date "+%Y-%m-%d %H:%M:%S")
+tim_dif=$(date "+%Y-%m-%d %H:%M:%S" -d "$sub_min")
 
-    - name: Set AWS access and secret keys
-      set_fact:
-        aws_access_key: "{{ aws_credential_key.aws_access_key }}"
-        aws_secret_key: "{{ aws_credential_key.aws_secret_key }}"
+# Initialize a variable to track if the exception was found
+EXCEPTION_FOUND=false
 
-    - name: Debug output of parameters (for troubleshooting)
-      debug:
-        msg: |
-          ALB Name: {{ alb_name }}
-          Environment: {{ playbook_choice }}
-          AWS Region: {{ aws_region }}
-          ALB Scheme: {{ alb_scheme }}
-          VPC ID: {{ VPC_ID }}
-          Security Groups: {{ security_groups }}
+# Loop through the latest 4 log files in the specified directory
+for LOGFILE in $(find "$EXCEPTION_LOG_PATH" -name "$EXCEPTION_LOGFILE" -type f -exec ls -t1 {} + | head -4); do
 
-    # Execute the specific sub-playbook based on the environment passed
-    - name: Execute sub-playbook based on environment
-      include_tasks: "tasks_for_{{ playbook_choice }}.yml"
-      vars:
-        alb_name: "{{ alb_name }}"
-        VPC_ID: "{{ VPC_ID }}"
-        alb_scheme: "{{ alb_scheme }}"
-        aws_region: "{{ aws_region }}"
-        security_groups: "{{ security_groups }}"
-      when: playbook_choice in ['DEV_CDE', 'DEV_NONCDE', 'QA_CDE', 'QA_NONCDE', 'PROD_CDE', 'PROD_NONCDE']
+    echo "Checking logs in file: $LOGFILE"
+
+    # Extract the records based on the timestamp within the specified time range
+    nrec=$(awk -v tim_dif="$tim_dif" -v tim_cur="$tim_cur" '
+    {
+        if ($0 ~ /[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}/) {
+            timestamp = substr($0, 1, 19)
+            if (timestamp >= tim_dif && timestamp <= tim_cur) {
+                print NR
+                exit
+            }
+        }
+    }' "$LOGFILE")
+
+    # Check if there are any records found
+    if [ -n "$nrec" ]; then
+
+        # Count the number of exceptions within the specified time range
+        EXCEPTION_COUNT=$(awk -v strec="$nrec" -v exception_string="$EXCEPTION_STRING" 'NR >= strec && $0 ~ exception_string { count++ } END { print count }' "$LOGFILE")
+
+        # Set EXCEPTION_FOUND to true if the exception count is greater than or equal to the threshold
+        if [ "$EXCEPTION_COUNT" -ge "$THRESHOLD" ]; then
+            EXCEPTION_FOUND=true
+            break
+        fi
+
+    fi
+done
+
+# Output the result for Ansible playbook to capture
+if [ "$EXCEPTION_FOUND" == true ]; then
+    echo "ALERT: $EXCEPTION_STRING found $EXCEPTION_COUNT times in the last $INTERVAL minutes."
+    exit 0
+else
+    echo "NO_ALERT: $EXCEPTION_STRING not found or less than $THRESHOLD times in the log files within the last $INTERVAL minutes."
+    exit 1
+fi
