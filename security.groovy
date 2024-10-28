968f0e9bf9a1c5f4a4bc54e705c7245e115e1472
IamKoofy <abhishekj.1c@gmail.com>
2024-10-28 23:52:38 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,16 +1,56 @@
-[defaults]
-callback_plugins = ./callback_plugins
+import json
+import requests
+from datetime import datetime
+from ansible.plugins.callback import CallbackBase
+import os
 
-[callback_whitelist]
-callbacks_enabled = cribl_callback
+class CallbackModule(CallbackBase):
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'notification'
+    CALLBACK_NAME = 'log_to_cribl'
 
+    def __init__(self):
+        super(CallbackModule, self).__init__()
+        self.cribl_endpoint = "https://default.main.vibrant-90msg.cribl.cloud:10070"
+        self.auth_token = "9999anf-annnnd-9444k-998885859004"
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.auth_token}'
+        }
+        self.user = None
+        self.template_name = None
 
+    def v2_playbook_on_start(self, playbook):
+        self.user = self._get_user()
+        self.template_name = playbook._file_name
+        log_entry = {
+            "user": self.user,
+            "template_name": self.template_name,
+            "event_type": "playbook_start",
+            "timestamp": datetime.utcnow().isoformat()
+        }
+        self.send_to_cribl(log_entry)
 
+    def v2_playbook_on_stats(self, stats):
+        summary = {host: stats.summarize(host) for host in stats.processed}
+        log_entry = {
+            "user": self.user,
+            "template_name": self.template_name,
+            "event_type": "playbook_end",
+            "timestamp": datetime.utcnow().isoformat(),
+            "summary": summary
+        }
+        self.send_to_cribl(log_entry)
 
----
-- name: Test Callback Plugin for Cribl
-  hosts: localhost
-  tasks:
-    - name: Print a message
-      ansible.builtin.debug:
-        msg: "This is a test for Cribl callback"
+    def send_to_cribl(self, log_data):
+        try:
+            print(f"Sending data to Cribl: {log_data}")  # Log the data being sent
+            response = requests.post(self.cribl_endpoint, headers=self.headers, json=log_data, verify=False)
+            print(f"Response status code: {response.status_code}")  # Log the response status code
+            if response.status_code != 200:
+                print(f"Failed to send log to Cribl: {response.text}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error while sending log to Cribl: {str(e)}")
+
+    def _get_user(self):
+        return os.getenv("AWX_USER_NAME", "unknown-user") or os.getenv("TOWER_USERNAME", "unknown-user")
