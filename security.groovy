8da38c9a320346c60caabb23d73474f3af386ca2
IamKoofy <abhishekj.1c@gmail.com>
2024-12-03 22:09:20 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,46 +1,11 @@
----
-- name: Tasks for DEV_NONCDE
-  amazon.aws.elb_application_lb:
-    name: "{{ alb_name }}"
-    state: present
-    region: "{{ aws_region }}"
-    subnets:
-      - "subnet-0744c28f0d343e071"
-      - "subnet-04b3546bad5bb8fa1"
-      - "subnet-09456a7aa413a0fa6"
-    security_groups:
-      - "sg-00cfac7b8c78f4014"
-    scheme: "{{ alb_scheme }}"
-    ip_address_type: ipv4
-    listeners:
-      - Protocol: HTTP
-        Port: 80
-        DefaultActions:
-          - Type: forward
-            TargetGroupName: Test-Target-group
-    when: alb_scheme == 'internal'
-
-- name: Tasks for Internet-Facing ALB
-  amazon.aws.elb_application_lb:
-    name: "{{ alb_name }}"
-    state: present
-    region: "{{ aws_region }}"
-    subnets:
-      - "subnet-0744c28f0d343e071"
-      - "subnet-04b3546bad5bb8fa1"
-      - "subnet-09456a7aa413a0fa6"
-    security_groups:
-      - "epaas30-internet-urls"
-    scheme: "{{ alb_scheme }}"
-    ip_address_type: ipv4
-    listeners:
-      - Protocol: HTTPS
-        Port: 443
-        DefaultActions:
-          - Type: forward
-            TargetGroupName: Test-Target-group
-        SslPolicy: ELBSecurityPolicy-2016-08
-        CertificateArn: "{{ certificate_arn }}"
-    when: alb_scheme == 'internet-facing'
-  vars:
-    certificate_arn: "{{ lookup('aws_acm', 'certificate_arn', domain_name=alb_domain_name) }}"
+- name: Send email notification for successful backup in production
+      mail:
+        host: localhost
+        port: 25
+        subject: "ROSA Backup Completed Successfully for {{ env_var }}"
+        body: |
+          Backup for the environment {{ env_var }} and namespaces {{ namespaces }} has been completed successfully.
+          Backup stored at: {{ backup_location }}
+        to: "{{ email_recipients | join(',') }}"
+      when: env_var in ['non-cde-prod', 'cde-prod']
+      delegate_to: localhost
