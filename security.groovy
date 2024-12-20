ca8b624d15dd9afdd0dab72e1ff99f6e1f6a20b0
IamKoofy <abhishekj.1c@gmail.com>
2024-12-20 22:19:00 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,24 +1,24 @@
-- name: Test ALB Creation
-  hosts: localhost
-  gather_facts: false
-  tasks:
-    - name: Create ALB
-      amazon.aws.elb_application_lb:
-        name: "test-alb"
-        state: present
-        region: "us-east-1"
-        subnets:
-          - "subnet-0a123456789abcde1"
-          - "subnet-0b123456789abcde2"
-        security_groups:
-          - "sg-0c123456789abcde3"
-        scheme: "internal"
-        ip_address_type: ipv4
-        listeners:
-          - Protocol: HTTP
-            Port: 80
-            DefaultActions:
-              - Type: forward
-                TargetGroupName: Test-Target-group
-        aws_access_key: "YOUR_ACCESS_KEY"
-        aws_secret_key: "YOUR_SECRET_KEY"
+<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <OutputType>Exe</OutputType>
+    <TargetFramework>net6.0</TargetFramework>
+    <RootNamespace>Profile.Sync.Organizer</RootNamespace>
+    <AssemblyName>Profile.Sync.Organizer</AssemblyName>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Include="Consul" Version="0.7.2.1" />
+    <PackageReference Include="EntityFramework" Version="6.1.3" />
+    <PackageReference Include="EventStore.ClientAPI" Version="3.9.4" />
+    <PackageReference Include="Newtonsoft.Json" Version="9.0.1" />
+    <PackageReference Include="RabbitMQ.Client" Version="4.1.1" />
+    <PackageReference Include="Serilog" Version="2.3.0" />
+    <PackageReference Include="Serilog.Formatting.Compact" Version="1.0.0" />
+    <PackageReference Include="SimpleInjector" Version="3.2.3" />
+    <PackageReference Include="StackExchange.Redis" Version="1.2.0" />
+    <PackageReference Include="Microsoft.AspNet.WebApi.Client" Version="5.2.3" />
+    <PackageReference Include="Topshelf" Version="4.0.3" />
+  </ItemGroup>
+
+</Project>
