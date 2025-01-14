5d63ccd737f3d4309745d47a88efede816a066dd
IamKoofy <abhishekj.1c@gmail.com>
2025-01-14 17:24:52 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,24 +1,17 @@
-<Project Sdk="Microsoft.NET.Sdk">
-
-  <PropertyGroup>
-    <OutputType>Exe</OutputType>
-    <TargetFramework>net6.0</TargetFramework>
-    <RootNamespace>Profile.Sync.Organizer</RootNamespace>
-    <AssemblyName>Profile.Sync.Organizer</AssemblyName>
-  </PropertyGroup>
-
-  <ItemGroup>
-    <PackageReference Include="Consul" Version="0.7.2.1" />
-    <PackageReference Include="EntityFramework" Version="6.1.3" />
-    <PackageReference Include="EventStore.ClientAPI" Version="3.9.4" />
-    <PackageReference Include="Newtonsoft.Json" Version="9.0.1" />
-    <PackageReference Include="RabbitMQ.Client" Version="4.1.1" />
-    <PackageReference Include="Serilog" Version="2.3.0" />
-    <PackageReference Include="Serilog.Formatting.Compact" Version="1.0.0" />
-    <PackageReference Include="SimpleInjector" Version="3.2.3" />
-    <PackageReference Include="StackExchange.Redis" Version="1.2.0" />
-    <PackageReference Include="Microsoft.AspNet.WebApi.Client" Version="5.2.3" />
-    <PackageReference Include="Topshelf" Version="4.0.3" />
-  </ItemGroup>
-
-</Project>
+steps:
+- task: PowerShell@1
+  displayName: 'Remove Existing Windows Service'
+  inputs:
+    scriptType: 'inlineScript'
+    script: |
+      $serviceName = "$(ComponentName)"
+      
+      # Check if the service exists
+      if (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) {
+          Write-Host "Service $serviceName exists. Stopping and removing it..."
+          Stop-Service -Name $serviceName -Force -ErrorAction SilentlyContinue
+          sc.exe delete $serviceName
+          Write-Host "Service $serviceName has been removed."
+      } else {
+          Write-Host "Service $serviceName does not exist. Skipping removal."
+      }
