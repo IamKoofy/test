b25a8058a04c582486beb05525494e6dbb3b135b
IamKoofy <abhishekj.1c@gmail.com>
2025-01-20 19:04:41 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,79 +1,53 @@
-# Directly referencing pipeline variables
-$serviceName = "$(ComponentName)"
-$serviceFolder = "$(ServiceRootFolder)\$(ComponentName)"
-$artifactFolder = "$(System.DefaultWorkingDirectory)\_$(ComponentName)-Build\$(ComponentName)"
-$serviceUsername = "$(ServiceUser)"
-$servicePassword = "$(ServicePassword)"
+---
+- name: Fetch SR Details and Validate
+  hosts: localhost
+  gather_facts: no
+  tasks:
+    - name: Process each SR
+      with_items: "{{ sr_ids }}"
+      loop_control:
+        loop_var: sr_id
+      tasks:
+        - name: Get details for each incident
+          uri:
+            url: "https://amexgbt.freshservice.com/api/v2/tickets/{{ sr_id }}/requested_items"
+            method: GET
+            url_username: qdIwrrVoGVjaN4HCpgC
+            url_password: <your_password_here>
+            return_content: yes
+            status_code: 200
+            validate_certs: no
+            body_format: json
+            force_basic_auth: yes
+            follow_redirects: all
+          register: sr_details
 
-if (!(Test-Path $artifactFolder)) {
-    Write-Error "No artifact folder exists at $artifactFolder"
-    Exit 1
-}
+        - name: Print results
+          debug:
+            msg: "Results: {{ sr_details }}"
 
-$serviceDll = "$serviceFolder\$serviceName.dll"
-$serviceExe = "$serviceFolder\$serviceName.exe"  # Path to your executable
+        - name: Parse the JSON content from ticket data
+          set_fact:
+            json_data: "{{ sr_details.content | from_json }}" # Updated from 'from_yaml' to 'from_json'
 
-if (Test-Path $serviceFolder) {
-    Write-Host "Service folder already exists, checking to stop and remove the service"
-    
-    $service = Get-Service $serviceName -ErrorAction SilentlyContinue
-    if ($service) {
-        Write-Host "Service exists."
-        Write-Host $service.Status
-        $serviceStates = "Running", "Paused"
-        if ($serviceStates -contains $service.Status) {
-            Write-Host "Stopping service"
-            Stop-Service $serviceName
-        }
-    }
+        - name: Extract fields from the JSON data
+          set_fact:
+            extracted_info: >-
+              {{ json_data.requested_items[0].custom_fields
+                 | dict2items
+                 | selectattr('key', 'in', ['environment', 'project', 'service'])
+                 | items2dict }}
 
-    if (Test-Path $serviceDll) {
-        Write-Host "Uninstalling service"
-        sc.exe delete $serviceName
-    }
+        - name: Print results
+          debug:
+            var: extracted_info
 
-    Write-Host "Stopping any existing processes"
-    Get-Process | foreach {
-        $processVar = $_
-        $_.Modules | foreach {
-            if ($_.FileName -eq $serviceDll) {
-                Stop-Process $processVar.id -Force
-            }
-        }
-    }
-
-    # Remove any locks on the files
-    Get-ChildItem $serviceFolder -Recurse | Unblock-File
-
-    # Remove all the items in the folder
-    Write-Host "Removing existing service files"
-    Remove-Item "$serviceFolder\*" -Recurse
-} else {
-    Write-Host "Creating $serviceFolder"
-    New-Item $serviceFolder -ItemType Directory -Force
-}
-
-# Copy files across from artifactFolder
-Write-Host "Copying service from artifactFolder to serviceFolder"
-Copy-Item "$artifactFolder\*" -Destination $serviceFolder -Recurse
-
-Write-Host "Installing service"
-# Install the service using sc.exe
-sc.exe create $serviceName binPath= "$serviceExe" start= auto
-
-# If needed, set service as delayed start
-Write-Host "Setting service as delayed start"
-sc.exe config $serviceName start= delayed-auto
-
-if (![string]::IsNullOrWhiteSpace($serviceUsername) -and $serviceUsername -ne "?") {
-    Write-Host "Setting service as user"
-    sc.exe config $serviceName obj= $serviceUsername password= $servicePassword
-}
-
-Write-Host "Setting service as delayed restart"
-sc.exe config $serviceName AppRestartDelay=5000
-
-$descr = "HRG - $serviceName"
-sc.exe description $serviceName "$descr"
-
-Write-Host "Service is installed and configured"
+        - name: Trigger Pod Restart for SR
+          ansible.builtin.include_role:
+            name: restart_pods
+          vars:
+            sr_environment: "{{ extracted_info.environment }}"
+            sr_project: "{{ extracted_info.project }}"
+            sr_service: "{{ extracted_info.service }}"
+            sr_restart_all: "{{ extracted_info.custom_fields.restart_all_pods }}"
+            sr_pod_names: "{{ extracted_info.custom_fields.pod_names }}"
