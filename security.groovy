f1f51bee168eeba733781cff94cfd43b7c0b96b2
IamKoofy <abhishekj.1c@gmail.com>
2025-01-20 21:26:34 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,3 +1,4 @@
+---
 - name: Fetch SR Details and Restart Pods
   hosts: localhost
   gather_facts: no
@@ -6,9 +7,9 @@
     auth_username: "qdIwrrVoGVjaN4HCpgC"
     auth_password: "<your_password_here>"
   tasks:
-    - name: Get details for each SR
+    - name: Fetch SR IDs
       uri:
-        url: "{{ api_url }}/{{ item }}/requested_items"
+        url: "{{ api_url }}"
         method: GET
         url_username: "{{ auth_username }}"
         url_password: "{{ auth_password }}"
@@ -18,48 +19,56 @@
         body_format: json
         force_basic_auth: yes
         follow_redirects: all
-      loop: "{{ sr_ids }}"  # Loop through each SR ID passed as extra vars
-      loop_control:
-        loop_var: sr_id
-      register: sr_details
-      failed_when: sr_details.status != 200
+      register: sr_ids_response
+      failed_when: sr_ids_response.status != 200
 
-    - name: Accumulate SR details into a list
+    - name: Extract SR IDs from the response
       set_fact:
-        all_srs_details: "{{ all_srs_details | default([]) + [item.json] }}"
-      loop: "{{ sr_details.results }}"
-      loop_control:
-        loop_var: item  # Use 'item' as the loop variable
-      register: accumulated_srs  # Store all accumulated data
+        sr_ids: "{{ sr_ids_response.json.tickets | map(attribute='id') | list }}"
 
-    - name: Extract fields from the JSON data
-      set_fact:
-        extracted_info: >-
-          {{ item.requested_items[0].custom_fields
-             | dict2items
-             | selectattr('key', 'in', ['environment', 'project', 'service'])
-             | items2dict }}
-      loop: "{{ accumulated_srs.results }}"  # Loop through all accumulated SR details
-      loop_control:
-        loop_var: item
-      register: extracted_info_list  # Store the extracted fields for each SR
+    - name: Process each SR
+      block:
+        - name: Get details for the incident
+          uri:
+            url: "{{ api_url }}/{{ item }}/requested_items"
+            method: GET
+            url_username: "{{ auth_username }}"
+            url_password: "{{ auth_password }}"
+            return_content: yes
+            status_code: 200
+            validate_certs: no
+            body_format: json
+            force_basic_auth: yes
+            follow_redirects: all
+          register: sr_details
+          failed_when: sr_details.status != 200
+          loop: "{{ sr_ids }}"  # Looping over the SR IDs
 
-    - name: Print extracted SR details
-      debug:
-        msg: "Extracted details: {{ item }}"
-      loop: "{{ extracted_info_list }}"  # Loop through the extracted info for all SRs
-      loop_control:
-        loop_var: item
+        - name: Parse the JSON content from ticket data
+          set_fact:
+            json_data: "{{ sr_details.json | default({}) }}"
 
-    - name: Restart Pods for each SR
-      ansible.builtin.include_role:
-        name: restart_pods
-      vars:
-        sr_environment: "{{ item.environment }}"
-        sr_project: "{{ item.project }}"
-        sr_service: "{{ item.service }}"
-        sr_restart_all: "{{ item.custom_fields.restart_all_pods }}"
-        sr_pod_names: "{{ item.custom_fields.pod_names }}"
-      loop: "{{ extracted_info_list }}"  # Loop through the extracted fields for all SRs
-      loop_control:
-        loop_var: item
+        - name: Extract fields from the JSON data
+          set_fact:
+            extracted_info: >-
+              {{
+                json_data.requested_items[0].custom_fields
+                 | dict2items
+                 | selectattr('key', 'in', ['environment', 'name_of_the_project', 'name_of_the_service'])
+                 | items2dict
+              }}
+
+        - name: Debug extracted SR details
+          debug:
+            msg: "Extracted details: {{ extracted_info }}"
+
+        - name: Restart Pods for the SR
+          ansible.builtin.include_role:
+            name: restart_pods
+          vars:
+            sr_environment: "{{ extracted_info.environment }}"
+            sr_project: "{{ extracted_info.name_of_the_project }}"
+            sr_service: "{{ extracted_info.name_of_the_service }}"
+            sr_restart_all: "{{ extracted_info.restart_all_pods | default(false) }}"
+            sr_pod_names: "{{ extracted_info.pod_names | default('') }}"
+      loop: "{{ sr_ids }}"
