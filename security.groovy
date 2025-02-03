4576bc365967c5d95bc73e3a5e427ed283c1bb50
IamKoofy <abhishekj.1c@gmail.com>
2025-02-03 16:55:54 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,1 +1,1 @@
-https://freshservice.com/api/v2/tickets/filter?query=%22(group_id:1234)%20AND%20(status:2%20OR%20status:6)%20AND%20(subject:%7E%22ePaaS%20Pod%20Start/Stop%22)%22&format=json
+https://freshservice.com/api/v2/tickets/filter?query=%22(group_id:1234)%20AND%20(status:2%20OR%20status:6)%20AND%20(subject%3A~%22ePaaS%20Pod%20Start%2FStop%22)%22&format=json
