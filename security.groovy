539e12b637c2d11244522446574d54d8a34a4818
IamKoofy <abhishekj.1c@gmail.com>
2025-02-03 16:58:11 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,1 +1,1 @@
-https://freshservice.com/api/v2/tickets/filter?query=%22(group_id:1234)%20AND%20(status:2%20OR%20status:6)%20AND%20(subject%3A~%22ePaaS%20Pod%20Start%2FStop%22)%22&format=json
+ AND (status:2 OR status:6) AND (subject:"*ePaaS Pod Start/Stop*")&format=json
