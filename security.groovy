23ce6090ecdbd4485022627cb97139092a02435b
IamKoofy <abhishekj.1c@gmail.com>
2025-02-03 17:00:30 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,1 +1,1 @@
- AND (status:2 OR status:6) AND (subject:"*ePaaS Pod Start/Stop*")&format=json
+api/v2/tickets/filter?query=(group_id:1234)%20AND%20(status:2%20OR%20status:6)%20AND%20(subject:%22*ePaaS%20Pod%20Start/Stop*%22)&format=json
