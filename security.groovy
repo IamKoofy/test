cf784cc2d0d75810f954b920a78ecd23f00b029d
IamKoofy <abhishekj.1c@gmail.com>
2025-02-11 22:08:06 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,59 +1,123 @@
-name: "Build AdService"
+name: Build .NET Framework App
 
 on:
-  workflow_dispatch:
-
-env:
-  GITHUB_PAT: ${{ secrets.GITHUB_PAT }}
-  SNYK_AUTH_TOKEN: ${{ secrets.SNYK_AUTH_TOKEN }}
-  SNYK_ORG: ${{ secrets.SNYK_ORG }}
-  SNYK_PROJECT_NAME: ${{ secrets.SNYK_PROJECT_NAME }}
-  MAJOR_VERSION: ${{ vars.MajorVersion }}
-  MINOR_VERSION: ${{ vars.MinorVersion }}
-  BUILD_NUMBER: ${{ vars.BuildNumber }}
-  REV: ${{ vars.Rev }}
-  PATH_TO_PROJECT: ${{ vars.PathToProject }}
-  SOLUTION_FILE: ${{ vars.SolutionFile }}
-  PATH_TO_TEST_PROJECT: ${{ vars.PathToTestProject }}
-  COMPONENT_NAME: ${{ vars.ComponentName }}
-  BUILD_CONFIGURATION: ${{ vars.BuildConfiguration }}
-  SONARQUBE_PROJECT_NAME: ${{ vars.SonarQubeProjectName }}
-  SONARQUBE_EXCLUSIONS: ${{ vars.SonarQubeExclusions }}
+  workflow_call:
+    inputs:
+      VersioningTaskGroupFilename:
+        required: false
+        type: string
+        default: 'task-groups/version-use-buildnumber.taskgroup.yml'
+      MajorVersion:
+        required: true
+        type: string
+      MinorVersion:
+        required: true
+        type: string
+      BuildNumber:
+        required: true
+        type: string
+      Rev:
+        required: true
+        type: string
+      SolutionFile:
+        required: true
+        type: string
+      PathToProject:
+        required: true
+        type: string
+      PathToTestProject:
+        required: true
+        type: string
+      ComponentName:
+        required: true
+        type: string
+      BuildConfiguration:
+        required: true
+        type: string
+      SnykScanEnabled:
+        required: false
+        type: boolean
+        default: true
+      SnykAuthToken:
+        required: false
+        type: string
+      SnykOrg:
+        required: false
+        type: string
+      SnykProjectName:
+        required: false
+        type: string
+      SonarQubeProjectName:
+        required: false
+        type: string
+      SonarQubeExclusions:
+        required: false
+        type: string
+      SonarQubeServiceConnection:
+        required: false
+        type: string
+        default: 'GBT Code Analysis'
+      VsTestEnabled:
+        required: false
+        type: boolean
+        default: false
 
 jobs:
   build:
-    name: "Building AdService"
-    runs-on: main-build-pool
+    runs-on: windows-latest
+
     steps:
-      - name: "Checkout Azure Pipelines Templates"
-        run: |
-          echo "Cloning Git templates"
-          git clone https://github.com/TEST/Azure-Pipelines.git templates
-          cd templates
-          git checkout main
+      - name: Checkout Repository
+        uses: actions/checkout@v4
 
-      - name: "Checkout MilkyWay-TravelConfirmation Repo"
-        run: |
-          echo "Cloning Git repo"
-          git clone https://${{ env.GITHUB_PAT }}@github.com/TEST/MilkyWay-TravelConfirmation.git MilkyWayTravelConfirmation
-          cd MilkyWayTravelConfirmation
-          git checkout main
+      - name: Display Pipeline Info
+        run: echo "Running .NET Framework build"
 
-      - name: "Build .NET Framework Application"
-        uses: ./.github/actions/build-dotnet-framework
+      - name: Run Versioning Task Group
+        run: echo "Running ${{ inputs.VersioningTaskGroupFilename }} with Version: ${{ inputs.MajorVersion }}.${{ inputs.MinorVersion }}.${{ inputs.BuildNumber }}.${{ inputs.Rev }}"
+
+      - name: SonarQube Pre-Build Analysis
+        run: echo "Running SonarQube analysis for project ${{ inputs.SonarQubeProjectName }}"
+
+      - name: Authenticate with NuGet Feed
+        run: echo "NuGet authentication completed"
+
+      - name: Restore NuGet Packages
+        run: nuget restore "${{ inputs.SolutionFile }}"
+
+      - name: Build .NET Framework Project
+        run: |
+          msbuild ${{ inputs.SolutionFile }} /p:Configuration=${{ inputs.BuildConfiguration }} /p:Platform="Any CPU"
+
+      - name: Run Snyk Security Scan
+        if: inputs.SnykScanEnabled == true
+        env:
+          SNYK_TOKEN: ${{ inputs.SnykAuthToken }}
+        run: |
+          echo "Running Snyk security scan for project ${{ inputs.SnykProjectName }}"
+
+      - name: Install Visual Studio Test Platform
+        run: echo "Installing Visual Studio Test Platform"
+
+      - name: Run Tests
+        if: inputs.VsTestEnabled == true
+        run: |
+          vstest.console.exe "${{ inputs.PathToTestProject }}\**\*test*.dll" /EnableCodeCoverage
+
+      - name: SonarQube Post-Build Analysis
+        run: echo "Finalizing SonarQube analysis for ${{ inputs.SonarQubeProjectName }}"
+
+      - name: Build Web Deploy Package
+        run: |
+          msbuild "${{ inputs.PathToProject }}" /p:Configuration=${{ inputs.BuildConfiguration }} /t:Package /p:OutputPath=$(Build.SourcesDirectory)\temp\ /p:PackageLocation=$(Build.SourcesDirectory)\Package\${{ inputs.ComponentName }}.zip
+
+      - name: Copy Component Files
+        run: |
+          mkdir -p ${{ github.workspace }}/artifacts/${{ inputs.ComponentName }}
+          cp -r Package/${{ inputs.ComponentName }}.zip temp/configuration.json ${{ github.workspace }}/artifacts/${{ inputs.ComponentName }}
+
+      - name: Upload Build Artifact
+        uses: actions/upload-artifact@v4
         with:
-          VersioningTaskGroupFilename: 'task-groups/version-build.taskgroup.yml'
-          MajorVersion: ${{ env.MAJOR_VERSION }}
-          MinorVersion: ${{ env.MINOR_VERSION }}
-          BuildNumber: ${{ env.BUILD_NUMBER }}
-          Rev: ${{ env.REV }}
-          PathToProject: ${{ env.PATH_TO_PROJECT }}
-          SolutionFile: ${{ env.SOLUTION_FILE }}
-          PathToTestProject: ${{ env.PATH_TO_TEST_PROJECT }}
-          ComponentName: ${{ env.COMPONENT_NAME }}
-          BuildConfiguration: ${{ env.BUILD_CONFIGURATION }}
-          SonarQubeProjectName: ${{ env.SONARQUBE_PROJECT_NAME }}
-          SonarQubeExclusions: ${{ env.SONARQUBE_EXCLUSIONS }}
-          SnykAuthToken: ${{ env.SNYK_AUTH_TOKEN }}
-          SnykOrg: ${{ env.SNYK_ORG }}
-          SnykProjectName: ${{ env.SNYK_PROJECT_NAME }}
+          name: ${{ inputs.ComponentName }}
+          path: ${{ github.workspace }}/artifacts/${{ inputs.ComponentName }}
