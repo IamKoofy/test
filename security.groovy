25503e88d2cd63ddf60d8825b83a032dd7dcbf8a
IamKoofy <abhishekj.1c@gmail.com>
2025-02-19 19:50:38 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,10 +1,6 @@
 name: templates_build_hrg_dotnet_framework
 
-templateinputs:
-  VersioningTaskGroupFilename:
-    required: false
-    default: task-groups/version-use-buildnumber.taskgroup.yml
-    type: string
+inputs:
   MajorVersion:
     required: false
     type: string
@@ -64,52 +60,53 @@ runs:
   using: composite
   steps:
     - uses: "./.github/actions/templates_common_task_groups_display_pipeline_info"
+
     - uses: "./.github/actions/templates_build_task_groups_version_use_buildnumbertaskgroup"
       with:
         MajorVersion: "${{ inputs.MajorVersion }}"
         MinorVersion: "${{ inputs.MinorVersion }}"
         BuildNumber: "${{ inputs.BuildNumber }}"
         Rev: "${{ inputs.Rev }}"
+
     - uses: "./.github/actions/templates_build_task_groups_sonarqube_pre_build_vstesttaskgroup"
       with:
         SonarQubeProjectName: "${{ inputs.SonarQubeProjectName }}"
         SonarQubeExclusions: "${{ inputs.SonarQubeExclusions }}"
         SonarQubeServiceConnection: "${{ inputs.SonarQubeServiceConnection }}"
-    
+
     - name: Authenticate with NuGet feed
-      uses: actions/setup-dotnet@v4.0.0
+      uses: actions/setup-dotnet@v4
       env:
         NUGET_AUTH_TOKEN: "${{ secrets.NUGET_AUTH_TOKEN }}"
       with:
         source-url: "https://repos.gbt.gbtad.com/repository/nuget-api-v3/index.json"
-    
+
     - name: Install Visual Studio Test Platform
-      run: |
-        nuget install Microsoft.TestPlatform -Source "https://repos.gbt.gbtad.com/repository/nuget-api-v3/index.json"
+      if: inputs.VsTestEnabled == 'true'
       shell: bash
-      if: inputs.VsTestEnabled == true
-    
+      run: nuget install Microsoft.TestPlatform -Source "https://repos.gbt.gbtad.com/repository/nuget-api-v3/index.json"
+
     - name: Restore NuGet packages
-      run: nuget restore ${{ inputs.SolutionFile }}
       shell: bash
-    
+      run: nuget restore ${{ inputs.SolutionFile }}
+
     - name: Install MSBuild
       uses: microsoft/setup-msbuild@v1.3.1
-    
+
     - name: Run MSBuild
-      run: msbuild ${{ github.workspace }}\${{inputs.SolutionFile}} -p:Configuration=${{ inputs.BuildConfiguration }} -p:Platform="Any CPU"
       shell: bash
-    
+      run: msbuild "${{ github.workspace }}/${{ inputs.SolutionFile }}" -p:Configuration=${{ inputs.BuildConfiguration }} -p:Platform="Any CPU"
+
     - uses: "./.github/actions/templates_build_task_groups_snyk_dotnet_scantaskgroup"
       with:
-        SnykScanEnabled: "${{inputs.SnykScanEnabled}}"
-        SnykSolutionFile: "${{inputs.SolutionFile}}"
-        SnykAuthToken: "${{inputs.SnykAuthToken}}"
-        SnykOrg: "${{inputs.SnykOrg}}"
-        SnykProjectName: "${{inputs.SnykProjectName}}"
-    
+        SnykScanEnabled: "${{ inputs.SnykScanEnabled }}"
+        SnykSolutionFile: "${{ inputs.SolutionFile }}"
+        SnykAuthToken: "${{ inputs.SnykAuthToken }}"
+        SnykOrg: "${{ inputs.SnykOrg }}"
+        SnykProjectName: "${{ inputs.SnykProjectName }}"
+
     - name: Run build tests
-      if: success() && inputs.VsTestEnabled == true
+      if: success() && inputs.VsTestEnabled == 'true'
       uses: microsoft/vstest-action@v1.0.0
       with:
         testAssembly: |
@@ -121,16 +118,22 @@ runs:
         vsTestVersion: toolsInstaller
         codeCoverageEnabled: true
         platform: "Any CPU"
-    
+
     - uses: "./.github/actions/sonarqube-post-build.taskgroup"
       with:
         SonarQubeProjectName: "${{ inputs.SonarQubeProjectName }}"
-    
+
     - name: Package Project
-      run: msbuild ${{ github.workspace }}\${{inputs.PathToProject}} /T:Package /P:OutputPath=${{ github.workspace }}\temp\ /P:PackageLocation=${{ github.workspace }}\Package\${{ env.ComponentName }}.zip -p:Configuration=${{ inputs.BuildConfiguration }} -p:Platform="Any CPU"
       shell: bash
-    
+      run: |
+        msbuild "${{ github.workspace }}/${{ inputs.PathToProject }}" /T:Package \
+          /P:OutputPath=${{ github.workspace }}/temp/ \
+          /P:PackageLocation=${{ github.workspace }}/Package/${{ inputs.ComponentName }}.zip \
+          -p:Configuration=${{ inputs.BuildConfiguration }} \
+          -p:Platform="Any CPU"
+
     - name: Publish Artifact
-      uses: actions/upload-artifact@v4.1.0
+      uses: actions/upload-artifact@v4
       with:
-        path: "${{ runner.temp }}\${{ env.ComponentName }}"
+        name: "${{ inputs.ComponentName }}"
+        path: "${{ github.workspace }}/Package/${{ inputs.ComponentName }}.zip"
