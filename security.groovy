ce23cfe5745205dd79e41f5e42c8918c3e922958
IamKoofy <abhishekj.1c@gmail.com>
2025-02-20 21:02:13 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,37 +1,57 @@
-name: templates_build_task_groups_sonarqube_post_buildtaskgroup
+name: templates_build_task_groups_sonarqube_pre_build_vstesttaskgroup
 
 inputs:
   SonarQubeProjectName:
     required: false
     type: string
+  SonarQubeExclusions:
+    required: false
+    type: string
+  SonarQubeServiceConnection:
+    required: false
+    default: GBT Code Analysis
+    type: string
 
 runs:
   using: composite
   steps:
-    - name: Remove sonar.branch.name Variable
-      continue-on-error: true
+    - name: Prepare Project Files for SonarQube Scan
       shell: powershell
       run: |
-        $sonarSettings= $Env:SONARQUBE_SCANNER_PARAMS | ConvertFrom-Json
-        $sonarSettings.PSObject.Properties.Remove("sonar.branch.name")
-        $updatedSonarSettings = $sonarSettings | ConvertTo-Json -compress
-        Write-Host "##vso[task.setvariable variable=SONARQUBE_SCANNER_PARAMS]$updatedSonarSettings"
-        Write-Host $updatedSonarSettings
+        function update-project-file($file) {
+          $fileContent = Get-Content -path $file.FullName
+          if ($fileContent -like ("*ProjectGuid*")) {
+            Write-Host "$($file.Name) already has a project id assigned"
+            return
+          }
+          [xml] $xmlFile = $fileContent
+          $node = $xmlFile.SelectSingleNode("//Project/PropertyGroup")
+          $child = $xmlFile.CreateElement("ProjectGuid")
+          $child.InnerText = "{"+[guid]::NewGuid().ToString().ToUpper()+"}"
+          $node.AppendChild($child)
+          $xmlFile.Save($file.FullName) | Out-Null
+          Write-Host "$($file.Name) has been assigned a new project id"
+        }
+        gci -path ${{ github.workspace }} -Filter *.csproj -Recurse | % { update-project-file $_ }
 
-    - name: Run SonarQube Analysis
-      continue-on-error: true
+    - name: Install SonarScanner for .NET
       shell: bash
       run: |
-        echo "Running SonarQube Analysis..."
-        sonar-scanner \
-          -Dsonar.projectKey=${{ inputs.SonarQubeProjectName }} \
-          -Dsonar.sources=. \
-          -Dsonar.host.url=$SONARQUBE_URL \
-          -Dsonar.login=$SONARQUBE_TOKEN
+        dotnet tool install --global dotnet-sonarscanner
+        echo "SonarScanner installed"
 
-    - name: Publish SonarQube Result
-      continue-on-error: true
+    - name: Prepare SonarQube for Scan
       shell: bash
       run: |
-        echo "Publishing SonarQube Result..."
-        curl -u $SONARQUBE_TOKEN: "$SONARQUBE_URL/api/qualitygates/project_status?projectKey=${{ inputs.SonarQubeProjectName }}"
+        echo "Preparing SonarQube for scan..."
+        dotnet-sonarscanner begin \
+          /k:"${{ inputs.SonarQubeProjectName }}" \
+          /d:sonar.host.url="$SONARQUBE_URL" \
+          /d:sonar.login="$SONARQUBE_TOKEN" \
+          /d:sonar.projectVersion="${{ github.run_number }}" \
+          /d:sonar.exclusions="${{ inputs.SonarQubeExclusions }}" \
+          /d:sonar.test.exclusions="${{ inputs.SonarQubeExclusions }}" \
+          /d:sonar.coverage.exclusions="**/*Tests*.cs,**/Models/**/*,**/Data/**/*,**/Program.cs,**/Startup.cs,${{ inputs.SonarQubeExclusions }}"
+      env:
+        SONARQUBE_URL: ${{ secrets.SONARQUBE_URL }}
+        SONARQUBE_TOKEN: ${{ secrets.SONARQUBE_TOKEN }}
