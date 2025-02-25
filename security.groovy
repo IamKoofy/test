6b129b20d959ae5051f08993dbdd133419749ecf
IamKoofy <abhishekj.1c@gmail.com>
2025-02-25 17:41:56 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,19 +1,3 @@
-- name: Load Pipeline Variables
-  id: load-vars
-  shell: pwsh
-  run: |
-    Write-Host "Loading pipeline variables from pipeline-vars.yml"
-    $yamlPath = "$env:GITHUB_WORKSPACE\pipeline-vars.yml"
+                      echo "::set-output name=$key::$value"  # Export to outputs
 
-    if (Test-Path $yamlPath) {
-      $content = Get-Content $yamlPath | Where-Object {$_ -match '^\s*[^#]'} # Ignore comments
-      foreach ($line in $content) {
-        $key, $value = $line -split ":\s*", 2
-        if ($key -and $value) {
-          echo "$key=$value" | Out-File -Append -Encoding utf8 $env:GITHUB_ENV
-        }
-      }
-    } else {
-      Write-Error "pipeline-vars.yml not found!"
-      exit 1
-    }
+needs.build-app.outputs
