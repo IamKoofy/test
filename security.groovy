ce6be7a1174c4f1bc9f28f18e2ca97103c3a28db
IamKoofy <abhishekj.1c@gmail.com>
2025-03-03 19:37:24 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,9 +1,18 @@
-- name: Package Project
-  shell: powershell
-  run: |
-    $projectPath = "${{ github.workspace }}\${{ inputs.PathToProject }}"
-    $outputPath = "${{ github.workspace }}\temp"
-    $packageLocation = "${{ github.workspace }}\Package\${{ inputs.ComponentName }}.zip"
-    $buildConfig = "${{ inputs.BuildConfiguration }}"
-    
-    msbuild $projectPath /T:Package /P:OutputPath=$outputPath /P:PackageLocation=$packageLocation /p:Configuration=$buildConfig /p:Platform="Any CPU"
+    - name: Upload artifact using GitHub API
+      shell: powershell
+      run: |
+        $ARTIFACT_NAME = "${{ inputs.name }}"
+        $ARTIFACT_PATH = "$env:GITHUB_WORKSPACE\artifacts\${{ inputs.name }}.zip"
+        
+        Write-Host "Uploading artifact $ARTIFACT_NAME..."
+        
+        Invoke-RestMethod -Uri "https://api.github.com/repos/${{ github.repository }}/actions/artifacts" `
+          -Method Post `
+          -Headers @{
+            Authorization = "Bearer ${{ inputs.token }}"
+            Accept = "application/vnd.github.v3+json"
+          } `
+          -Form @{
+            name = $ARTIFACT_NAME
+            file = Get-Item -Path $ARTIFACT_PATH
+          }
