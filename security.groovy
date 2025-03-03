7fc4ec25d6ce5e181bc81f0ec1fe3c022dd4d5b4
IamKoofy <abhishekj.1c@gmail.com>
2025-03-03 17:31:10 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,6 +1,17 @@
-- name: Set .NET Environment Variables
-  shell: powershell
-  run: |
-    $dotnetPath = "C:\Program Files\dotnet"
-    [System.Environment]::SetEnvironmentVariable("DOTNET_ROOT", $dotnetPath, [System.EnvironmentVariableTarget]::User)
-    Write-Host "DOTNET_ROOT set for the user.
+name: Setup MSBuild Internal
+description: Sets up MSBuild on Windows agents
+runs:
+  using: composite
+  steps:
+    - name: Add MSBuild to PATH
+      shell: powershell
+      run: |
+        Write-Host "Setting up MSBuild"
+        $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin"
+        if (Test-Path $msbuildPath) {
+          echo "MSBuild found at: $msbuildPath"
+          echo "$msbuildPath" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+        } else {
+          Write-Error "MSBuild not found in the expected location."
+          exit 1
+        }
