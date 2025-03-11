fb726d8a393f0400b6ecb9c8aac41211d7f111cb
IamKoofy <abhishekj.1c@gmail.com>
2025-03-11 20:36:00 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,17 +1,11 @@
-remove-dangling-images/action.yml
-name: 'Remove Dangling Docker Images'
-description: 'Removes any unwanted dangling images from the Docker system'
-
-runs:
-  using: "composite"
-  steps:
-    - name: Remove Dangling Images
-      shell: pwsh
-      run: |
-        $danglingImages = docker images -f "dangling=true" -q
-        if ($danglingImages) {
-          $danglingImages | ForEach-Object {
-            Write-Host "Removing $_"
-            docker rmi $_ -f
-          }
-        }
+E1OpenShiftUrl: ${{ secrets.ROSA_NONCDE_DEV_URL }}
+E1OpenShiftToken: ${{ secrets.DEV_ROSA_TOKEN }}
+DeployToCert: true
+E2OpenShiftUrl: ${{ secrets.ROSA_NONCDE_CERT_URL }}
+E2OpenShiftToken: ${{ secrets.CERT_ROSA_TOKEN }}
+OpenShiftProjectName: my-openshift-project
+DeploymentConfigName: my-deployment-config
+ContainerName: my-container
+DockerDevRepo: my-docker-dev-repo
+DockerCertRepo: my-docker-cert-repo
+CertApprovalEmail: test.Mustafa@myorg.com
