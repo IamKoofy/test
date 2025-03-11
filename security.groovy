6d44b68a7806201da0e2b27731d5b2d7f86fbad7
IamKoofy <abhishekj.1c@gmail.com>
2025-03-11 16:51:56 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,5 +1,32 @@
-- name: Run Yarn Build
-  shell: pwsh
-  run: |
-    $env:PATH="C:\Program Files\Volta;$env:PATH"
-    yarn run build
+name: "Docker Build"
+description: "Composite action to build a Docker image"
+
+inputs:
+  Dockerfile:
+    required: true
+    type: string
+  DockerRepo:
+    required: true
+    type: string
+  DockerImageName:
+    required: true
+    type: string
+  DockerVersionArgs:
+    required: true
+    type: string
+  DockerContext:
+    required: true
+    type: string
+  DockerFolder:
+    required: false
+    type: string
+    default: 'C:\DockerShare'
+
+runs:
+  using: "composite"
+  steps:
+    - name: Build Docker Image
+      shell: pwsh
+      run: |
+        docker context use "${{ inputs.DockerContext }}"
+        docker build -f "${{ inputs.Dockerfile }}" -t "${{ inputs.DockerRepo }}/${{ inputs.DockerImageName }}:${{ inputs.DockerVersionArgs }}" "${{ inputs.DockerFolder }}"
