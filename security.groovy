a435bf4859f0eed6ffb9cd992653572f84d9bae2
IamKoofy <abhishekj.1c@gmail.com>
2025-03-11 16:28:21 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,18 +1,5 @@
-- name: Version the Application
-      shell: pwsh
-      run: |
-        $buildNumber = "${{ inputs.BuildNumber }}"
-        $rev = "${{ inputs.Rev }}"
-        
-        if ($buildNumber.Trim().ToLower() -eq 'auto') {
-          $year = (Get-Date -Format "yy")
-          $dayNumber = (Get-Date).DayOfYear.ToString().PadLeft(3, '0')
-          $buildNumber = "$year$dayNumber"
-        }
-
-        if ($rev.Trim().ToLower() -eq 'auto') {
-          $rev = "${{ github.run_number }}"
-        }
-
-        $newBuildNumber = "${{ inputs.MajorVersion }}.${{ inputs.MinorVersion }}.$buildNumber.$rev"
-        Write-Host "New Build Number: $newBuildNumber"
+- name: Run Yarn Build
+  shell: pwsh
+  run: |
+    $env:PATH="C:\Program Files\Volta;$env:PATH"
+    yarn run build
