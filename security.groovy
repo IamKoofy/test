4c3133e8803a492df341eec0697ba5b2dd6dde16
IamKoofy <abhishekj.1c@gmail.com>
2025-03-21 22:57:42 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,8 +1,1 @@
-- task: PowerShell@2
-  displayName: Print All Files in Workspace
-  inputs:
-    targetType: inline
-    workingDirectory: '$(Build.SourcesDirectory)'
-    script: |
-      Write-Host "Listing all files in the workspace:"
-      Get-ChildItem -Recurse | ForEach-Object { Write-Host $_.FullName }
+$env:NODE_OPTIONS="--experimental-global-webcrypto"
