3b3fc9eee6c7b2a1d35f5f0a04073a5763751688
IamKoofy <abhishekj.1c@gmail.com>
2025-03-21 23:09:33 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,1 +1,24 @@
-node -e "globalThis.crypto = require('crypto').webcrypto;"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+
+export default defineConfig({
+  define: {
+    'globalThis.crypto': 'require("crypto").webcrypto'
+  },
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) =>
+            ["tableau-viz", "tableau-authoring-viz", "tableau-ask-data", "viz-filter", "VizFilter"].includes(tag),
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
