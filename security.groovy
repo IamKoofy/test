8914ad332aa2bbd61fa1077b42145382b906474b
IamKoofy <abhishekj.1c@gmail.com>
2025-04-04 16:40:39 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,63 +1,117 @@
-name: Build Nightly
+name: 'DotNet Core Docker V2'
+description: 'Composite GitHub Action for .NET Core build and Docker pipeline'
+inputs:
+  MajorVersion:
+    required: true
+  MinorVersion:
+    required: true
+  BuildNumber:
+    required: true
+  Rev:
+    required: true
+  SolutionFile:
+    required: true
+  PathToMainProject:
+    required: true
+  PathToTestProject:
+    required: true
+  DotnetBuildOutputDockerSrc:
+    required: true
+  BuildArgs:
+    required: false
+    default: '-c Release'
+  Dockerfile:
+    required: true
+  DockerVersionArgs:
+    required: false
+    default: '${{ github.run_number }}'
+  DockerRepo:
+    required: true
+  DockerImageName:
+    required: true
+  SonarQubeProjectName:
+    required: true
+  SonarQubeExclusions:
+    required: true
+  SonarQubeCoverletReportPaths:
+    required: true
+  SonarQubeServiceConnection:
+    required: false
+    default: 'GBT Code Analysis'
+  SnykScanEnabled:
+    required: false
+    default: 'true'
+  SnykAuthToken:
+    required: true
+  SnykOrg:
+    required: true
+  SnykProjectName:
+    required: true
 
-on:
-  schedule:
-    - cron: "20 2 * * 1-5"  # Runs at 2:20 AM Monday to Friday
-  workflow_dispatch:  # Allows manual trigger
+runs:
+  using: "composite"
+  steps:
 
-env:
-  MajorVersion: ${{ secrets.MAJOR_VERSION }}
-  MinorVersion: ${{ secrets.MINOR_VERSION }}
-  BuildNumber: ${{ github.run_number }}
-  Rev: ${{ github.run_attempt }}
-  SolutionFile: ${{ secrets.SOLUTION_FILE }}
-  PathToMainProject: ${{ secrets.MAIN_PROJECT_PATH }}
-  PathToTestProject: ${{ secrets.TEST_PROJECT_PATH }}
-  DockerFile: ${{ secrets.DOCKER_FILE }}
-  DockerRepo: ${{ secrets.DOCKER_DEV_REPO }}
-  DockerImageName: ${{ secrets.DOCKER_IMAGE_NAME }}
-  SonarQubeProjectName: ${{ secrets.SONAR_PROJECT_NAME }}
-  SonarQubeExclusions: ${{ secrets.SONAR_EXCLUSION }}
-  SonarQubeCoverletReportPaths: ${{ secrets.COVERLET_REPORT_PATHS }}
-  DotnetBuildOutputDockerSrc: ${{ secrets.DOTNET_BUILD_OUTPUT_DOCKER_SRC }}
-  SnykAuthToken: ${{ secrets.SNYK_AUTH_TOKEN }}
-  SnykOrg: ${{ secrets.SNYK_ORG }}
-  SnykProjectName: ${{ secrets.SNYK_PROJECT_NAME }}
-  SnykScanEnabled: ${{ secrets.SNYK_SCAN_ENABLED }}
+    - uses: DotNet-COP/display-pipeline-info@main
 
-jobs:
-  build:
-    runs-on: self-hosted  # Use your GitHub self-hosted runner
+    - uses: DotNet-COP/version-build@main
+      with:
+        MajorVersion: ${{ inputs.MajorVersion }}
+        MinorVersion: ${{ inputs.MinorVersion }}
+        BuildNumber: ${{ inputs.BuildNumber }}
+        Rev: ${{ inputs.Rev }}
 
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v4
+    - uses: DotNet-COP/sonarqube-pre-build-dotnet@main
+      with:
+        SonarQubeProjectName: ${{ inputs.SonarQubeProjectName }}
+        SonarQubeExclusions: ${{ inputs.SonarQubeExclusions }}
+        SonarQubeCoverletReportPaths: ${{ inputs.SonarQubeCoverletReportPaths }}
+        SonarQubeServiceConnection: ${{ inputs.SonarQubeServiceConnection }}
 
-      - name: Checkout templates repository
-        uses: actions/checkout@v4
-        with:
-          repository: DotNet-COP/devops-templates
-          ref: Development
-          token: ${{ secrets.GIT_TOKEN }}
+    - name: Build Core Project
+      shell: pwsh
+      run: |
+        dotnet build "${{ inputs.SolutionFile }}" ${{ inputs.BuildArgs }}
 
-      - name: Run Build Process
-        uses: ./.github/actions/build/dotnet-core-docker-v2
-        with:
-          MajorVersion: ${{ env.MajorVersion }}
-          MinorVersion: ${{ env.MinorVersion }}
-          BuildNumber: ${{ env.BuildNumber }}
-          Rev: ${{ env.Rev }}
-          SolutionFile: ${{ env.SolutionFile }}
-          PathToMainProject: ${{ env.PathToMainProject }}
-          PathToTestProject: ${{ env.PathToTestProject }}
-          DockerFile: ${{ env.DockerFile }}
-          DockerRepo: ${{ env.DockerRepo }}
-          DockerImageName: ${{ env.DockerImageName }}
-          SonarQubeProjectName: ${{ env.SonarQubeProjectName }}
-          SonarQubeExclusions: ${{ env.SonarQubeExclusions }}
-          SonarQubeCoverletReportPaths: ${{ env.SonarQubeCoverletReportPaths }}
-          DotnetBuildOutputDockerSrc: ${{ env.DotnetBuildOutputDockerSrc }}
-          SnykAuthToken: ${{ env.SnykAuthToken }}
-          SnykOrg: ${{ env.SnykOrg }}
-          SnykProjectName: ${{ env.SnykProjectName }}
-          SnykScanEnabled: ${{ env.SnykScanEnabled }}
+    - uses: DotNet-COP/snyk-dotnet-scan@main
+      with:
+        SnykScanEnabled: ${{ inputs.SnykScanEnabled }}
+        SnykAuthToken: ${{ inputs.SnykAuthToken }}
+        SnykSolutionFile: ${{ inputs.SolutionFile }}
+        SnykOrg: ${{ inputs.SnykOrg }}
+        SnykProjectName: ${{ inputs.SnykProjectName }}
+
+    - name: Run Tests
+      shell: pwsh
+      run: |
+        dotnet test "${{ inputs.PathToTestProject }}" ${{ inputs.BuildArgs }} --filter "(TestCategory=Unit)|(TestCategory=Component)" --collect "Code Coverage" /p:EnableNETAnalyzers=false
+
+    - uses: DotNet-COP/sonarqube-post-build@main
+      with:
+        SonarQubeProjectName: ${{ inputs.SonarQubeProjectName }}
+
+    - name: Publish Core Project
+      shell: pwsh
+      run: |
+        dotnet publish "${{ inputs.PathToMainProject }}" --no-build --no-restore -o d:/DockerShare/${{ github.run_id }}${{ inputs.DotnetBuildOutputDockerSrc }} ${{ inputs.BuildArgs }}
+
+    - uses: DotNet-COP/docker-build@main
+      with:
+        Dockerfile: ${{ inputs.Dockerfile }}
+        DockerRepo: ${{ inputs.DockerRepo }}
+        DockerImageName: ${{ inputs.DockerImageName }}
+        DockerVersionArgs: ${{ inputs.DockerVersionArgs }}
+        DockerContext: 'docker-unix'
+        DockerFolder: 'd:/DockerShare/${{ github.run_id }}'
+
+    - name: Clean up Docker Share
+      shell: pwsh
+      continue-on-error: true
+      run: |
+        Remove-Item -Recurse -Force "d:/DockerShare/${{ github.run_id }}"
+
+    - uses: DotNet-COP/docker-push@main
+      with:
+        DockerRepo: ${{ inputs.DockerRepo }}
+        DockerImageName: ${{ inputs.DockerImageName }}
+        DockerVersionArgs: ${{ inputs.DockerVersionArgs }}
