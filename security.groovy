98d5d9e0affaa43a5d6e83adac32bc26003b0549
IamKoofy <abhishekj.1c@gmail.com>
2025-04-04 20:19:37 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,4 +1,6 @@
 - name: Copy Dockerfile to Docker context
-  shell: pwsh
+  shell: powershell
   run: |
-    Copy-Item "${{ inputs.Dockerfile }}" "d:/DockerShare/${{ github.run_id }}/Dockerfile"
+    $targetFolder = "D:\DockerShare\${{ runner.id }}\${{ github.run_id }}"
+    New-Item -ItemType Directory -Path $targetFolder -Force | Out-Null
+    Copy-Item "${{ inputs.Dockerfile }}" "$targetFolder\Dockerfile"
