1356ea8f47bfc3017aa876d7b59d707686cfbc03
IamKoofy <abhishekj.1c@gmail.com>
2025-04-15 22:14:34 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,2 +1,8 @@
-namespaces: "{{ namespaces_json.stdout | from_json | json_query('items[?metadata.labels[\"project-owner\"]==`gbt`].metadata.name') }}"
+- name: Extract namespaces where project-owner == "gbt"
+  set_fact:
+    namespaces: >-
+      {{ namespaces_json.stdout | from_json
+        | json_query('items[*].{name: metadata.name, labels: metadata.labels}') 
+        | selectattr('labels.project-owner', 'equalto', 'gbt') 
+        | map(attribute='name') | list }}
   when: oc_login_result.rc == 0
