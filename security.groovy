490fcf1bfdffd0f6d5a7a08f2d9835987350d560
IamKoofy <abhishekj.1c@gmail.com>
2025-04-23 20:52:06 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,15 +1,9 @@
- - name: List files in .sonarqube\out directory
-        shell: pwsh
-        run: |
-          $sonarOutDir = ".sonarqube\out"
-          
-          # Check if the directory exists
-          if (Test-Path $sonarOutDir) {
-              Write-Host "Directory exists: $sonarOutDir"
-              
-              # List all files in the directory
-              Write-Host "Listing files in $sonarOutDir:"
-              Get-ChildItem -Path $sonarOutDir
-          } else {
-              Write-Host "Directory does not exist: $sonarOutDir"
-          }
+$reportPath = ".sonarqube\out\report-task.txt"
+    $targetPath = "$env:RUNNER_TEMP\report-task.txt"
+
+    if (Test-Path $reportPath) {
+      Copy-Item $reportPath $targetPath
+      Write-Host "Copied report to temp location: $targetPath"
+    } else {
+      Write-Warning "report-task.txt not found"
+    }
