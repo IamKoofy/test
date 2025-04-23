21d64101946a1009c090fca8e24b55ad5d8cd37d
IamKoofy <abhishekj.1c@gmail.com>
2025-04-23 20:38:35 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,1 +1,15 @@
-Get-ChildItem -Recurse -Directory -Force | Where-Object { $_.Name -eq ".sonarqube" }
+ - name: List files in .sonarqube\out directory
+        shell: pwsh
+        run: |
+          $sonarOutDir = ".sonarqube\out"
+          
+          # Check if the directory exists
+          if (Test-Path $sonarOutDir) {
+              Write-Host "Directory exists: $sonarOutDir"
+              
+              # List all files in the directory
+              Write-Host "Listing files in $sonarOutDir:"
+              Get-ChildItem -Path $sonarOutDir
+          } else {
+              Write-Host "Directory does not exist: $sonarOutDir"
+          }
