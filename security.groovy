afbcc7e841185cc1cc4687303754dc4ee7cb39fc
IamKoofy <abhishekj.1c@gmail.com>
2025-04-23 16:53:46 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,1 +1,18 @@
-oc get namespaces -l project-owner=gbt -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
+dotnet tool install --global dotnet-sonarscanner
+$env:PATH += ";$env:USERPROFILE\.dotnet\tools"
+
+dotnet sonarscanner begin `
+  /k:"$env:ProjectKey" `
+  /d:sonar.host.url="$env:SonarQubeUrl" `
+  /d:sonar.login="$env:SonarToken" `
+  /d:sonar.exclusions="$env:Exclusions" `
+  /d:sonar.cs.opencover.reportsPaths="$env:CoverageReport"
+
+dotnet build "$env:SolutionFile" $env:BuildArgs
+
+dotnet test "$env:TestProject" $env:BuildArgs `
+  --filter "(TestCategory=Unit)|(TestCategory=Component)" `
+  --collect "Code Coverage" `
+  /p:EnableNETAnalyzers=false
+
+dotnet sonarscanner end /d:sonar.login="$env:SonarToken"
