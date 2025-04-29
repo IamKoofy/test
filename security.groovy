07be74c3cd7f1931a42e1a5cd98d8c0accc320da
IamKoofy <abhishekj.1c@gmail.com>
2025-04-29 20:42:10 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,1 +1,34 @@
-"body": "Dear user,<br><br>Your request for pod restart has been successfully completed. Please find the details below:<br><br><b>PROJECT:</b> {{ sr_project }}<br><b>SERVICE:</b> {{ sr_service }}<br><b>PODS RESTARTED:</b> {{ pods_before_restart.stdout }}<br><br>Regards,<br>TEAM",
+# .github/workflows/release.yml
+name: SBP Admin Web Release to Dev
+
+on:
+  workflow_run:
+    workflows: ["SBP Admin Web Build"]
+    types:
+      - completed
+
+jobs:
+  deploy-dev:
+    if: ${{ github.event.workflow_run.conclusion == 'success' }}
+    runs-on: ubuntu-latest
+    env:
+      OpenshiftProjectName: sbp-dev
+      DeploymentConfigName: sbp-admin-web
+      ContainerName: sbp-admin-web
+      DockerImageName: sbp-admin-web
+      DockerDevRepo: registry.dev.mycorp.com/sbp
+
+    steps:
+    - name: Set image tag from build run
+      run: |
+        echo "IMAGE_TAG=${{ github.event.workflow_run.head_branch }}-${{ github.event.workflow_run.run_number }}" >> $GITHUB_ENV
+
+    - name: Log in to OpenShift (Dev)
+      run: |
+        oc login ${{ secrets.ROSA_DEV_URL }} --token=${{ secrets.ROSA_DEV_TOKEN }}
+        oc project ${{ env.OpenshiftProjectName }}
+
+    - name: Patch DeploymentConfig with new image
+      run: |
+        oc patch dc/${{ env.DeploymentConfigName }} --patch \
+          "{\"spec\": {\"template\": {\"spec\": {\"containers\": [{\"name\": \"${{ env.ContainerName }}\",\"image\": \"${{ env.DockerDevRepo }}/${{ env.DockerImageName }}:${{ env.IMAGE_TAG }}\"}]}}}}"
