4d1f86335afb2d614336f7306fcb4cad279594d4
IamKoofy <abhishekj.1c@gmail.com>
2025-05-14 21:34:07 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,8 +1,6 @@
-- name: Configure NuGet to use Azure Artifacts feed
-        run: |
-          dotnet nuget add source ^
-            --name "hrgtec" ^
-            --username "buildagent" ^
-            --password "${{ secrets.AZURE_DEVOPS_PAT }}" ^
-            --store-password-in-clear-text ^
-            "https://hrgtec.pkgs.visualstudio.com/_packaging/hrgtec/nuget/v3/index.json"
+- name: Ensure clean NuGet source
+  shell: pwsh
+  run: |
+    if (dotnet nuget list source | Select-String "hrgtec") {
+      dotnet nuget remove source "hrgtec"
+    }
