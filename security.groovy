803724ec55602efe8a57181ae0bef037aaa0fd21
IamKoofy <abhishekj.1c@gmail.com>
2025-05-14 21:44:38 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,6 +1,14 @@
-- name: Ensure clean NuGet source
+- name: Ensure clean NuGet source by URL
   shell: pwsh
   run: |
-    if (dotnet nuget list source | Select-String "hrgtec") {
-      dotnet nuget remove source "hrgtec"
+    $sourceUrl = "https://hrgtec.pkgs.visualstudio.com/_packaging/hrgtec/nuget/v3/index.json"
+    $existingSourceName = dotnet nuget list source | ForEach-Object {
+      if ($_ -match "^\s*(\*?)\s*([^\s]+)\s+\[$sourceUrl\]") {
+        return $matches[2]
+      }
+    }
+
+    if ($existingSourceName) {
+      Write-Host "Removing existing NuGet source: $existingSourceName"
+      dotnet nuget remove source $existingSourceName
     }
