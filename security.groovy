335f5e504c1807876d774fa9b626bcbcc506a2a7
IamKoofy <abhishekj.1c@gmail.com>
2025-05-16 19:48:46 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -20,25 +20,35 @@ steps:
       function update-project-file($file)
       {
           $fileContent = Get-Content -path $file.FullName
-          if ($fileContent -like "*ProjectGuid*")
+          if ($fileContent -like "*<ProjectGuid>*")
           {
               Write-Host "$($file.Name) already has a project id assigned"
-              return 
+              return $false
           }
 
           [xml]$xmlFile = $fileContent
           $node = $xmlFile.SelectSingleNode("//Project/PropertyGroup")
+          if (-not $node) {
+              Write-Warning "$($file.Name): No <PropertyGroup> found"
+              return $false
+          }
+
           $child = $xmlFile.CreateElement("ProjectGuid")
           $child.InnerText = "{" + [guid]::NewGuid().ToString().ToUpper() + "}"
           $node.AppendChild($child)
           $xmlFile.Save($file.FullName) | Out-Null
           Write-Host "$($file.Name) has been assigned a new project id"
+          return $true
       }
 
       $projectRoot = "$(Build.SourcesDirectory)\${{ parameters.gitFolder }}"
       Write-Host "Scanning project root: $projectRoot"
-      Get-ChildItem -Path $projectRoot -Filter *.csproj -Recurse -File | ForEach-Object {
-          update-project-file $_
+
+      $csprojFiles = Get-ChildItem -Path $projectRoot -Filter *.csproj -Recurse -File
+      foreach ($file in $csprojFiles) {
+          if ($file -ne $null) {
+              update-project-file $file | Out-Null
+          }
       }
 
 - task: SonarQubePrepare@4
@@ -50,8 +60,8 @@ steps:
     projectKey: '${{ parameters.SonarQubeProjectName }}'
     projectVersion: '$(Build.BuildNumber)'
     extraProperties: |
-      sonar.exclusions= ${{ parameters.SonarQubeExclusions }}
-      sonar.test.exclusions= ${{ parameters.SonarQubeExclusions }}
+      sonar.exclusions=${{ parameters.SonarQubeExclusions }}
+      sonar.test.exclusions=${{ parameters.SonarQubeExclusions }}
       sonar.coverage.exclusions=**/*Tests*.cs,**/Models/**/*,**/Data/**/*,**/Program.cs,**/Startup.cs,${{ parameters.SonarQubeExclusions }}
       sonar.cs.roslyn.ignoreIssues=false
 ##      sonar.cs.opencover.reportsPaths=${{ parameters.SonarQubeCoverletReportPaths }}
