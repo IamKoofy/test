5832b6572bdc0c705554fa05721ee1520833f5d4
IamKoofy <abhishekj.1c@gmail.com>
2025-05-27 19:17:40 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,67 +1,12 @@
-parameters:
-  - name: SonarQubeProjectName
-    type: string
-  - name: SonarQubeExclusions
-    type: string    
-  - name: SonarQubeCoverletReportPaths
-    type: string
-  - name: SonarQubeServiceConnection
-    type: string
-    default: 'GBT Code Analysis'
-  - name: gitFolder
-    type: string
-
-steps:
-- task: PowerShell@2
-  displayName: 'prep proj files for sonarqube scan'
-  inputs:
-    targetType: 'inline'
-    script: |
-      function update-project-file($file)
-      {
-          $fileContent = Get-Content -path $file.FullName
-          if ($fileContent -like "*<ProjectGuid>*")
-          {
-              Write-Host "$($file.Name) already has a project id assigned"
-              return $false
-          }
-
-          [xml]$xmlFile = $fileContent
-          $node = $xmlFile.SelectSingleNode("//Project/PropertyGroup")
-          if (-not $node) {
-              Write-Warning "$($file.Name): No <PropertyGroup> found"
-              return $false
-          }
-
-          $child = $xmlFile.CreateElement("ProjectGuid")
-          $child.InnerText = "{" + [guid]::NewGuid().ToString().ToUpper() + "}"
-          $node.AppendChild($child)
-          $xmlFile.Save($file.FullName) | Out-Null
-          Write-Host "$($file.Name) has been assigned a new project id"
-          return $true
-      }
-
-      $projectRoot = "$(Build.SourcesDirectory)\${{ parameters.gitFolder }}"
-      Write-Host "Scanning project root: $projectRoot"
-
-      $csprojFiles = Get-ChildItem -Path $projectRoot -Filter *.csproj -Recurse -File
-      foreach ($file in $csprojFiles) {
-          if ($file -ne $null) {
-              update-project-file $file | Out-Null
-          }
-      }
-
-- task: SonarQubePrepare@4
-  displayName: 'prep sonarqube for scan'
-  continueOnError: true
-  inputs:
-    SonarQube: '${{ parameters.SonarQubeServiceConnection }}'
-    scannerMode: 'MSBuild'
-    projectKey: '${{ parameters.SonarQubeProjectName }}'
-    projectVersion: '$(Build.BuildNumber)'
-    extraProperties: |
-      sonar.exclusions=${{ parameters.SonarQubeExclusions }}
-      sonar.test.exclusions=${{ parameters.SonarQubeExclusions }}
-      sonar.coverage.exclusions=**/*Tests*.cs,**/Models/**/*,**/Data/**/*,**/Program.cs,**/Startup.cs,${{ parameters.SonarQubeExclusions }}
-      sonar.cs.roslyn.ignoreIssues=false
-##      sonar.cs.opencover.reportsPaths=${{ parameters.SonarQubeCoverletReportPaths }}
+- name: Configure Nexus NuGet Source (Anonymous)
+  shell: powershell
+  run: |
+    $sourceName = "nexus-nuget"
+    $sourceUrl = "https://nuget.repo.com/repository/nuget-group"
+    $existingSources = dotnet nuget list source
+    if ($existingSources -match $sourceUrl) {
+      Write-Host "NuGet source already exists: $sourceUrl"
+    } else {
+      dotnet nuget add source --name $sourceName $sourceUrl
+      Write-Host "Added Nexus NuGet source: $sourceUrl"
+    }
