7bfd47c1110607f1b6ec88dc65e66560f7194d9f
IamKoofy <abhishekj.1c@gmail.com>
2025-06-10 22:21:29 +0530

    Update security.groovy

--- security.groovy
+++ security.groovy
@@ -1,4 +1,5 @@
 variables:
+  - group: GithubPAT  # Fixed typo from "GihubPAT"
   - name: BuildParameters.ArtifactName
     value: drop
   - name: BuildPlatform
@@ -7,121 +8,108 @@ variables:
     value: 'Release'
   - name: Build.BuildId
     value: $(date:yyyyMMdd)$(rev:.r)
-  - group: GithubPAT  # Ensure this variable group has 'GithubPAT' defined
 
 jobs:
-- job: Job_1
-  displayName: Build Job
-  pool:
-    name: main-build-pool
-  variables:
-    System.Debug: true
-  steps:
+  - job: Job_1
+    displayName: Agent job 1
+    pool:
+      name: main-build-pool
 
-  - task: PowerShell@2
-    displayName: 'Clone GitHub Repo'
-    inputs:
-      targetType: 'inline'
-      script: |
-        $ErrorActionPreference = 'Stop'
+    steps:
+      - task: PowerShell@2
+        displayName: 'Clone GitHub Repo'
+        inputs:
+          targetType: 'inline'
+          script: |
+            $ErrorActionPreference = 'Stop'
 
-        $token = "$(GithubPAT)"
-        if (-not $token) {
-            throw "GitHub PAT is missing!"
-        }
+            $token = "$(GithubPAT)"
+            if (-not $token) {
+              throw "GitHub PAT is missing!"
+            }
 
-        $repo = "AEGBT/gbt-apac-ABM.EmailOffers"
-        $branch = "dev"
-        $localPath = "APAC-ABM-PROD"
+            $repo = "AEGBT/gbt-apac-PAM-Production"
+            $branch = "ABM-Dev"
+            $localPath = "gbt-apac-PAM"
 
-        $gitUrl = "https://$token@github.com/$repo.git"
+            $gitUrl = "https://$token@github.com/$repo.git"
 
-        Write-Host "Cloning from $gitUrl to $localPath..."
-        git clone -b $branch $gitUrl $localPath
+            Write-Host "Cloning from $gitUrl to $localPath..."
+            git clone -b $branch $gitUrl $localPath
 
-        Write-Host "Repo cloned successfully. Listing files:"
-        Get-ChildItem -Path $localPath -Recurse | ForEach-Object { Write-Host $_.FullName }
+            Write-Host "Listing cloned files:"
+            Get-ChildItem $localPath -Recurse | ForEach-Object {
+              Write-Host $_.FullName
+            }
 
-  - task: SonarQubePrepare@5
-    displayName: Prepare SonarQube Analysis
-    inputs:
-      SonarQube: 3c7512f3-eb4b-41d5-918c-a632d362014d
-      projectKey: gbtapac-emailoffers-prod
-      projectName: gbtapac-emailoffers-prod
-      extraProperties: |
-        sonar.projectBaseDir=$(System.DefaultWorkingDirectory)/APAC-ABM-PROD
+      - task: SonarQubePrepare@5
+        displayName: Prepare analysis on SonarQube
+        enabled: false  # still disabled, but fixed path
+        inputs:
+          SonarQube: 1ecdfa54-774a-4892-81fd-4d31642bc912
+          projectKey: gbt-apac-PAM
+          projectName: gbt-apac-PAM
+          extraProperties: |
+            sonar.projectBaseDir=$(System.DefaultWorkingDirectory)/gbt-apac-PAM
 
-  - task: NuGetToolInstaller@1
-    inputs:
-      versionSpec: '>=6.6.1'
+      - task: NuGetCommand@2
+        displayName: NuGet restore
+        inputs:
+          solution: gbt-apac-PAM\PAM.sln
+          selectOrConfig: config
+          nugetConfigPath: gbt-apac-PAM\nuget.config
+          externalEndpoints: 9d6b3432-5c32-4a0e-8620-9a06ff19f0d8
 
-  - task: NuGetCommand@2
-    displayName: Restore NuGet packages
-    inputs:
-      solution: APAC-ABM-PROD\ABM.EmailOffers.sln
-      selectOrConfig: config
-      nugetConfigPath: APAC-ABM-PROD\nuget.config
-      externalEndpoints: 9d6b3432-5c32-4a0e-8620-9a06ff19f0d8
+      - task: VSBuild@1
+        displayName: Build solution
+        inputs:
+          solution: gbt-apac-PAM\PAM.sln
+          msbuildArgs: >
+            /p:DeployOnBuild=true 
+            /p:WebPublishMethod=Package 
+            /p:PackageAsSingleFile=true 
+            /p:SkipInvalidConfigurations=true 
+            /p:PackageLocation="$(build.artifactstagingdirectory)\\"
+          platform: $(BuildPlatform)
+          configuration: $(BuildConfiguration)
 
-  - task: VSBuild@1
-    inputs:
-      solution: APAC-ABM-PROD\ABM.EmailOffers.sln
-      msbuildArgs: >
-        /p:DeployOnBuild=true 
-        /p:WebPublishMethod=Package 
-        /p:PackageAsSingleFile=true 
-        /p:SkipInvalidConfigurations=true 
-        /p:PackageLocation="$(build.artifactstagingdirectory)\\"
-      platform: $(BuildPlatform)
-      configuration: $(BuildConfiguration)
+      - task: VSTest@2
+        displayName: Test Assemblies
+        inputs:
+          testAssemblyVer2: >
+            gbt-apac-PAM\**\bin\$(BuildConfiguration)\**\*test*.dll
+            !**\obj\**
+          platform: $(BuildPlatform)
+          configuration: $(BuildConfiguration)
 
-  - task: VisualStudioTestPlatformInstaller@1
-    inputs:
-      versionSelector: specificVersion
-      testPlatformVersion: '>=6.6.1'
+      - task: SonarQubeAnalyze@5
+        displayName: Run Code Analysis
+        enabled: false
 
-  - task: VSTest@2
-    displayName: Run Unit Tests
-    inputs:
-      testAssemblyVer2: >
-        APAC-ABM-PROD\**\bin\$(BuildConfiguration)\**\*test*.dll
-        !**\obj\**
-        !**\ref\**
-        !**\xunit*.dll
-      vsTestVersion: toolsInstaller
-      runSettingsFile: APAC-ABM-PROD\EmailOfferUnittest.runsettings
-      codeCoverageEnabled: true
+      - task: SonarQubePublish@5
+        displayName: Publish Quality Gate Result
+        enabled: false
 
-  - task: PublishTestResults@2
-    displayName: Publish Test Results
-    inputs:
-      testRunner: VSTest
-      testResultsFiles: '$(System.DefaultWorkingDirectory)\APAC-ABM-PROD\**\TestResults\*.trx'
+      - task: PublishSymbols@2
+        displayName: Publish symbols path
+        continueOnError: true
+        inputs:
+          SearchPattern: '**\bin\**\*.pdb'
+          PublishSymbols: false
+          SymbolServerType: TeamServices
 
-  - task: SonarQubeAnalyze@5
-    displayName: Run SonarQube Code Analysis
-    inputs:
-      extraProperties: |
-        sonar.working.directory=.sonarqube
+      - task: Application security testing@2021
+        displayName: Application security testing
+        enabled: false
+        inputs:
+          projectName: ABM-MASTER
+          CheckmarxService: 6ac90be7-3609-417a-bd5f-da024882fa3b
+          fullTeamName: CxServer\SP\Company\Users\GG-APACAPPTEAMCHECKMARX
 
-  - task: SonarQubePublish@5
-    displayName: Publish SonarQube Results
-    continueOnError: true
-
-  - task: NexusIqPipelineTask@1
-    displayName: Nexus IQ Policy Evaluation
-    continueOnError: true
-    inputs:
-      nexusIqService: e80e497e-f9fd-4dd0-bf06-53020dfcb9e3
-      organizationId: e2d8254992b344f79a5ad89bce5e7e71
-      applicationId: ABM.EMailOffers
-      stage: Release
-      scanTargets: |
-        $(Build.ArtifactStagingDirectory)/**/*.dll
-        $(Build.ArtifactStagingDirectory)/**/*.exe
-
-  - task: PublishBuildArtifacts@1
-    displayName: Publish Build Artifact
-    inputs:
-      PathtoPublish: '$(Build.ArtifactStagingDirectory)'
-      ArtifactName: '$(BuildParameters.ArtifactName)'
+      - task: PublishBuildArtifacts@1
+        displayName: Publish Artifact
+        condition: succeededOrFailed()
+        inputs:
+          PathtoPublish: $(Build.ArtifactStagingDirectory)
+          ArtifactName: $(BuildParameters.ArtifactName)
+          TargetPath: '\\my\share\$(Build.DefinitionName)\$(Build.BuildNumber)'
